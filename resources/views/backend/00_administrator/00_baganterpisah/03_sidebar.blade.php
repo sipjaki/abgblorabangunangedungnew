5f47bb942106eda2c4dfd999ffcb0ff883701216
SipjakiBlora <sipjakiblora@gmail.com>
2025-06-10 02:04:13 +0700

    e

--- resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
@@ -681,33 +681,18 @@
             <li class="nav-header" style="color: white;">MAIN MENU</li>
             <li class="nav-item">
 
-                <a href="#" class="nav-link">
-                    <!-- Main Building Icon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="url(#deepseek-gradient)" class="nav-icon" viewBox="0 0 16 16">
-                        <defs>
-                            <linearGradient id="deepseek-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
-                                <stop offset="0%" style="stop-color:#0066FF;stop-opacity:1" />
-                                <stop offset="100%" style="stop-color:#00BFFF;stop-opacity:1" />
-                            </linearGradient>
-                        </defs>
-                        <path d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
-                        <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
-                    </svg>
-                    <p style="color: white; margin-left: 8px;">
-                        PBG/SLF
-                        <i class="nav-arrow bi bi-chevron-right" style="margin-left: auto;"></i>
-                    </p>
-                </a>
+              <a href="#" class="nav-link">
+    <!-- Ganti SVG dengan Bootstrap Icon -->
+    <i class="bi bi-building nav-icon" style="color: white; font-size: 16px;"></i>
+
+    <p style="color: white; margin-left: 8px;">
+        PBG/SLF
+        <i class="nav-arrow bi bi-chevron-right" style="margin-left: auto;"></i>
+    </p>
+</a>
+
                 <ul class="nav nav-treeview" style="margin-left: 15px;">
-                    {{-- <li class="nav-item">
-                        <a href="/bestrukturdinas" class="nav-link">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="url(#deepseek-gradient)" class="nav-icon" viewBox="0 0 16 16">
-                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-                                <path d="M8 8a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
-                            </svg>
-                            <p style="color: white; margin-left: 8px;">Informasi PBG</p>
-                        </a>
-                    </li> --}}
+
                     <li class="nav-item">
                         <a href="/bestrukturdinas" class="nav-link">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="url(#deepseek-gradient)" class="nav-icon" viewBox="0 0 16 16">
