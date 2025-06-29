72b930e4c2bf573d2c357b7656a8ca18e9c3fead
SipjakiBlora <sipjakiblora@gmail.com>
2025-06-30 03:22:22 +0700

    e

--- resources/views/frontend/android/00_fiturmenu/04_menunavigasi.blade.php
+++ resources/views/frontend/android/00_fiturmenu/04_menunavigasi.blade.php
@@ -93,14 +93,14 @@
         </div>
       </a>
 
-      <a href="#">
+      {{-- <a href="#">
         <div class="flex flex-col items-center gap-2">
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/android/menunavigasi/012.png" class="w-full h-full object-cover" alt="icon" loading="lazy">
           </div>
           <p class="font-medium text-sm text-[#757C98] leading-[21px]">SPPD</p>
         </div>
-      </a>
+      </a> --}}
 
       <a href="/web">
         <div class="flex flex-col items-center gap-2">
