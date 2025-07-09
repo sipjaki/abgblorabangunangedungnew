1e237565a178960152fc433a08fbe9f5553196f6
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-09 13:45:28 +0700

    e

--- resources/views/frontend/android/01_halamanutama/index.blade.php
+++ resources/views/frontend/android/01_halamanutama/index.blade.php
@@ -8,7 +8,7 @@
       <img src="/assets/android/iconmenu/bangunangedungapp.jpg" class="absolute object-cover w-full h-full" alt="backgrounds" loading="lazy">
 
       <div class="fixed top-0 w-full max-w-[640px] px-[18px] z-30" style="margin-top: -25px;">
-<nav class="bg-white p-3 sm:p-[10px_16px] h-fit w-full flex items-center justify-between rounded-full shadow-[0_8px_30px_0_#0A093212] z-10 mt-[60px]">
+<nav style="background-color: rgba(255, 255, 255, 0.3); backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px);" class="p-3 sm:p-[10px_16px] h-fit w-full flex items-center justify-between rounded-full shadow-[0_8px_30px_0_#0A093212] z-10 mt-[60px]">
   <!-- Logo Kiri -->
   <a href="signup.html" class="shrink-0">
     <div class="w-12 h-12 sm:w-[54px] sm:h-[54px] flex overflow-hidden rounded-full items-center justify-center">
@@ -17,19 +17,19 @@
   </a>
 
   <!-- Teks Tengah -->
- <div class="flex-1 mx-2 sm:mx-4 min-w-0">
-  <div class="flex flex-col items-center sm:items-start text-center sm:text-left">
-    <p class="font-semibold text-sm sm:text-base leading-tight text-[#4041DA] truncate w-full" style="font-size:12px;">
-      ABG Blora Bangunan Gedung
-    </p>
-    <div class="flex items-center justify-center sm:justify-start">
-      <p class="font-semibold text-sm sm:text-base leading-tight whitespace-normal" style="font-size:12px;">
-        Dinas Pekerjaan Umum Dan
-        <br> Penataan Ruang <br> Kabupaten Blora Jawa Tengah
+  <div class="flex-1 mx-2 sm:mx-4 min-w-0">
+    <div class="flex flex-col items-center sm:items-start text-center sm:text-left">
+      <p class="font-semibold text-sm sm:text-base leading-tight text-[#4041DA] truncate w-full" style="font-size:12px;">
+        ABG Blora Bangunan Gedung
       </p>
+      <div class="flex items-center justify-center sm:justify-start">
+        <p class="font-semibold text-sm sm:text-base leading-tight whitespace-normal" style="font-size:12px;">
+          Dinas Pekerjaan Umum Dan
+          <br> Penataan Ruang <br> Kabupaten Blora Jawa Tengah
+        </p>
+      </div>
     </div>
   </div>
-</div>
 
   <!-- Logo Kanan -->
   <a href="" class="shrink-0">
