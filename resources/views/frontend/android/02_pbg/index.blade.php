ef84f5b4457ceb35185e1a2c0e4bc820b1c9ed4e
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-10 01:16:21 +0700

    e

--- resources/views/frontend/android/02_pbg/index.blade.php
+++ resources/views/frontend/android/02_pbg/index.blade.php
@@ -106,7 +106,7 @@
               </div>
             </a>
 
-            <a href="/404" class="bg-white rounded-xl flex flex-col p-4 hover:shadow-md transition">
+            <a href="/slffungsiusaha" class="bg-white rounded-xl flex flex-col p-4 hover:shadow-md transition">
               <div class="flex items-center gap-3 p-4 rounded-lg border border-[#DCDFE6]">
                 <div class="w-[60px] h-[60px] flex shrink-0 rounded-lg overflow-hidden">
                   <img src="/assets/android/pbgslf/SLF_FUNGSI_USAHA.png" class="object-cover w-full h-full" alt="thumbnail">
