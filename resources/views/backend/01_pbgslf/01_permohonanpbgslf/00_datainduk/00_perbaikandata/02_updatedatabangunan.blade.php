7166a4bbe6b71501ad31ff3a6c84585e4cb26ce0
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-11 02:38:15 +0700

    e

--- resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/00_perbaikandata/02_updatedatabangunan.blade.php
+++ resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/00_perbaikandata/02_updatedatabangunan.blade.php
@@ -383,7 +383,7 @@ th {
 </h5>
     <hr class="my-4" style="border-top: 2px dashed #0d6efd; width: 60%; margin: auto;">
 </div>
-<form id="formPemilik" action="{{ route('bepbgdatabangunanupdate', $data->id) }}" method="POST">
+<form id="formPemilik" action="{{ route('updatedatabangunannew', $data->id) }}" method="POST">
     @csrf
     @method('PUT')
     <input type="hidden" name="pbgslfbangunan_id" value="{{ $data->pbgslfbangunan_id }}">
