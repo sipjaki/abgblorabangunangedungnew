bb950511995e0b61c9f1d193558917c1961c1187
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-12 23:28:29 +0700

    ergreg

--- resources/views/frontend/abgblora/04_bantuanteknis/02_informasibantek.blade.php
+++ resources/views/frontend/abgblora/04_bantuanteknis/02_informasibantek.blade.php
@@ -460,40 +460,8 @@
             }
         }
     </style>
-<!-- Pastikan sudah include Bootstrap Icons -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
-<div style="overflow-x: auto; white-space: nowrap; padding: 10px 0;">
-  <div style="display: inline-flex; gap: 10px; font-family: 'Poppins', sans-serif;">
-    <a href="/infobantek" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-info-circle"></i> Informasi Umum
-    </a>
-    <a href="/infobanteklampiran" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-gear"></i> Lampiran & Referensi
-    </a>
-    <a href="/infobantekpetunjuk" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-geo-alt"></i> Petunjuk Umum
-    </a>
-    <a href="/infobantekasistensi" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-list-ul"></i> Asistensi
-    </a>
-    <a href="/infobantekpeneliti" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-paperclip"></i> Penliti Kontrak
-    </a>
-    <a href="/infobantekperhitungan" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-paperclip"></i> Penyusutan Nilai
-    </a>
-    <a href="/infobantekpemeliharaan" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-paperclip"></i> Pemeliharaan & Konstruksi
-    </a>
-    <a href="/infobantekpendampingan" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-paperclip"></i> Pendampingan Serah Terima
-    </a>
-    <a href="/infobantektimteknis" class="button-abgblora" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
-      <i class="bi bi-paperclip"></i> Tim Teknis
-    </a>
-  </div>
-</div>
+@include('frontend.abgblora.04_bantuanteknis.fiturmenu')
 
 <style>
         :root {
