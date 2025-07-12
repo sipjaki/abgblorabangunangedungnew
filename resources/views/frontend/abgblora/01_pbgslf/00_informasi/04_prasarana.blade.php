5030d35aac6e67ee19b693148d53086fa3e4ecfc
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-13 01:38:34 +0700

    eg

--- resources/views/frontend/abgblora/01_pbgslf/00_informasi/04_prasarana.blade.php
+++ resources/views/frontend/abgblora/01_pbgslf/00_informasi/04_prasarana.blade.php
@@ -189,9 +189,6 @@
 <!-- Tambahkan link Poppins jika belum -->
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
-<!-- Konten PBG Fungsi Prasarana -->
-<div style="max-width: 900px; margin: 40px auto; padding: 30px; background: #fff; border-radius: 12px; box-shadow: 0 4px 10px rgba(0,0,0,0.08); font-family: 'Poppins', sans-serif; font-size: 15px; color: #333; line-height: 1.8;">
-
   <!-- Judul -->
   <h2 style="font-size: 20px; font-weight: 600; margin-bottom: 20px; color: #1a1a1a;">
     Persetujuan Bangunan Gedung (PBG) - Fungsi Prasarana
@@ -248,7 +245,6 @@
     <li>Penerbitan dokumen PBG oleh DPMPTSP</li>
   </ol>
 
-</div>
 
 
                                         </div>
