7c8b791db7cd5dfe8f0190b67c8e13352a6dc80c
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-12 22:30:04 +0700

    e

--- resources/views/frontend/android/04_bantuanteknis/02_formpermohonanbantek.blade.php
+++ resources/views/frontend/android/04_bantuanteknis/02_formpermohonanbantek.blade.php
@@ -208,7 +208,7 @@
     <input type="hidden" name="dinas_id" value="{{ $dinas_id }}">
 
     <!-- Section 1: Jenis Permohonan -->
-    {{-- <div class="form-section"> --}}
+    <div class="form-section">
         <div class="section-header">
             <i class="fas fa-info-circle"></i>
             <strong>Jenis Permohonan Bantuan Teknis Saudara!</strong>
