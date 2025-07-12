611867ab69828fb07ee318ab2c90d8519b5c61a2
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-12 17:18:03 +0700

    e

--- resources/views/backend/05_agendapelatihan/03_uploadmateripel.blade.php
+++ resources/views/backend/05_agendapelatihan/03_uploadmateripel.blade.php
@@ -167,7 +167,7 @@ th {
                         <button class="button-kembali"
                         type="button"
 
-                        onclick="location.href='{{ url('/bebantekpemohonasistensi') }}';"
+                        onclick="location.href='{{ url('/beagendapelatihanabg') }}';"
                         style="cursor: pointer; color:black; margin-left:5px;">
                         <i class="bi bi-arrow-left" style="margin-right: 5px;"></i> Kembali
                         </button>
