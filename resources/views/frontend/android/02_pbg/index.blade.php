a03d5e0f64c5b677052a49de315cb37a2c56abbf
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-12 21:05:33 +0700

    e

--- resources/views/frontend/android/02_pbg/index.blade.php
+++ resources/views/frontend/android/02_pbg/index.blade.php
@@ -2,7 +2,7 @@
 
 <body class="font-poppins text-[#070625]">
   <section id="content" class="max-w-[640px] w-full min-h-screen mx-auto flex flex-col bg-[#F8F8F8] overflow-x-hidden pb-[120px] relative">
-    <div class="w-full h-[184px] absolute top-0 bg-cover bg-center" style="background-image: url('/assets/android/iconmenu/belakanggit status.jpg');">
+    <div class="w-full h-[184px] absolute top-0 bg-cover bg-center" style="background-image: url('/assets/android/iconmenu/belakang.jpg');">
     </div>
      <div class="relative z-10 flex flex-col gap-6 mt-[60px]">
       <div class="top-menu flex justify-between items-center px-[18px]">
