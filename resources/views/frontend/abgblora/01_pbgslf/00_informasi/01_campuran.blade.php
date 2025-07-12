fd41d18705c2e7e22f096df522865ed9bceb9af8
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-13 01:08:00 +0700

    eg

--- resources/views/frontend/abgblora/01_pbgslf/00_informasi/01_campuran.blade.php
+++ resources/views/frontend/abgblora/01_pbgslf/00_informasi/01_campuran.blade.php
@@ -179,7 +179,7 @@
     @include('backend.00_administrator.00_baganterpisah.09_button')
 
     <!-- Banner Section -->
-    <section class="header-banner" style="margin-top: 75px;">
+    <section class="header-banner" style="margin-top: 65px;">
         <div class="container max-w-[1130px] mx-auto" style="padding-top: 50px;">
             <div class="flex items-center gap-[20px]">
                 <!-- Content here -->
