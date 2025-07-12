f9fb3947e6788f795a6c023321a63c438282bb36
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-12 23:29:01 +0700

    e

--- resources/views/frontend/abgblora/04_bantuanteknis/fiturmenu.blade.php
+++ resources/views/frontend/abgblora/04_bantuanteknis/fiturmenu.blade.php
@@ -1,31 +1,31 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-<div style="overflow-x: auto; white-space: nowrap; padding: 10px 0;">
+<div style="overflow-x: auto; black-space: nowrap; padding: 10px 0;">
   <div style="display: inline-flex; gap: 10px; font-family: 'Poppins', sans-serif;">
-    <a href="/infobantek" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobantek" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-info-circle"></i> Informasi Umum
     </a>
-    <a href="/infobanteklampiran" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobanteklampiran" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-gear"></i> Lampiran & Referensi
     </a>
-    <a href="/infobantekpetunjuk" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobantekpetunjuk" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-geo-alt"></i> Petunjuk Umum
     </a>
-    <a href="/infobantekasistensi" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobantekasistensi" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-list-ul"></i> Asistensi
     </a>
-    <a href="/infobantekpeneliti" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobantekpeneliti" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-paperclip"></i> Penliti Kontrak
     </a>
-    <a href="/infobantekperhitungan" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobantekperhitungan" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-paperclip"></i> Penyusutan Nilai
     </a>
-    <a href="/infobantekpemeliharaan" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobantekpemeliharaan" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-paperclip"></i> Pemeliharaan & Konstruksi
     </a>
-    <a href="/infobantekpendampingan" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobantekpendampingan" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-paperclip"></i> Pendampingan Serah Terima
     </a>
-    <a href="/infobantektimteknis" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; white-space: nowrap;">
+    <a href="/infobantektimteknis" class="button-baru" style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: black; border-radius: 8px; cursor: pointer; display: flex; align-items: center; gap: 8px; text-decoration: none; black-space: nowrap;">
       <i class="bi bi-paperclip"></i> Tim Teknis
     </a>
   </div>
