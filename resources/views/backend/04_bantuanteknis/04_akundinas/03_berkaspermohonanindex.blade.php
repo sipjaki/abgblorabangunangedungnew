912c0a3e1a499f327c2024605bf2232835ba9e47
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-12 23:58:08 +0700

    e

--- resources/views/backend/04_bantuanteknis/04_akundinas/03_berkaspermohonanindex.blade.php
+++ resources/views/backend/04_bantuanteknis/04_akundinas/03_berkaspermohonanindex.blade.php
@@ -229,7 +229,7 @@ th {
              {{-- @canany(['superadmin', 'admin']) --}}
     @canany(['superadmin', 'admin'])
     <a href="{{ route('bebantuanteknisindexmenu') }}">
-        <button class="button-kembali button-abgblora" type="button"
+        <button class="button-baru" type="button"
             style="cursor: pointer; margin-left:5px; color:black;">
             <i class="bi bi-arrow-left" style="margin-right: 5px;"></i> Kembali
         </button>
@@ -343,7 +343,7 @@ th {
 
             <td style="text-align: center;">
                 <a href="{{ route('beperhitunganpenyusutan.show', $item->id) }}"
-                    class="button-kembali"
+                    class="button-baru"
                     style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: black; border: none; transition: background-color 0.3s, color 0.3s;"
                     onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.border='1px solid #6c757d';"
                     onmouseout="this.style.backgroundColor='#6c757d'; this.style.color='white'; this.style.border='none';">
@@ -357,7 +357,7 @@ th {
     <div style="display: flex; justify-content: center;">
 
                 <a href="{{ route('bebanteklapper3.show', $item->id) }}"
-                    class="button-kembali"
+                    class="button-baru"
                     style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: black; border: none; transition: background-color 0.3s, color 0.3s;"
                     onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.border='1px solid #6c757d';"
                     onmouseout="this.style.backgroundColor='#6c757d'; this.style.color='white'; this.style.border='none';">
@@ -372,7 +372,7 @@ th {
 <td style="text-align: center;">
     <div style="display: flex; justify-content: center;">
     <button type="button"
-        class="button-kembali"
+        class="button-baru"
         data-bs-toggle="modal"
         data-bs-target="#modalLihatBerkas{{ $item->id }}"
         style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: black; border: none; transition: background-color 0.3s, color 0.3s;"
