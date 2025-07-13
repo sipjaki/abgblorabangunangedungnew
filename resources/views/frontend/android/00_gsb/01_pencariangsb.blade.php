9e0577e6b328b4a71f494446dd635a65a7e8d4ee
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-13 17:43:46 +0700

    e

--- resources/views/frontend/android/00_gsb/01_pencariangsb.blade.php
+++ resources/views/frontend/android/00_gsb/01_pencariangsb.blade.php
@@ -45,7 +45,7 @@
     <div class="mb-8">
         <label for="searchInput" class="block mb-2 font-semibold text-sm text-gray-700">Ketik atau pilih ruas jalan:</label>
         <div class="relative">
-            <input list="jalanOptions" id="searchInput"
+            <input list="jalanOptions" id="searchInput" style="height: 25px;"
                    class="w-full px-5 py-4 border border-blue-300 rounded-xl shadow-md
                           focus:outline-none focus:ring-2 focus:ring-blue-500
                           focus:border-transparent text-base"
