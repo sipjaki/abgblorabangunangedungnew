55e501a03cc889f3f68d974f1c986f4377cde7ca
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-14 15:25:01 +0700

    e

--- resources/views/backend/11_perjalanandinas/04_suratperjalanandinas.blade.php
+++ resources/views/backend/11_perjalanandinas/04_suratperjalanandinas.blade.php
@@ -134,6 +134,17 @@ th {
     </div>
 @endcanany
 
+@canany(['internal'])
+    <div style="display: flex; justify-content: flex-end; margin-bottom:5px;">
+
+       <a href="{{ url()->previous() }}">
+    <button class="button-newvalidasi" type="button" style="cursor: pointer; color:white;">
+        <i class="bi bi-arrow-left" style="margin-right: 5px;"></i> Kembali
+    </button>
+</a>
+    </div>
+@endcanany
+
 <br>
 <br>
       <hr>
