5bb63b633578fca823b0c9ee7cd75a868351eedf
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-14 13:26:38 +0700

    e

--- resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/00_perbaikandata/06_updatedatastruktur.blade.php
+++ resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/00_perbaikandata/06_updatedatastruktur.blade.php
@@ -384,7 +384,7 @@ th {
     <hr class="my-4" style="border-top: 2px dashed #0d6efd; width: 60%; margin: auto;">
 </div>
 
-<form id="formPemilik" action="{{ route('updatedataarsitekturnew', $data->id) }}" method="POST">
+<form id="formPemilik" action="{{ route('updatedatastrukturnew', $data->id) }}" method="POST">
     @csrf
     <input type="hidden" name="pbgslfbangunan_id" value="{{ $data->id }}">
     <input type="hidden" name="id" value="{{ $data->id }}">
