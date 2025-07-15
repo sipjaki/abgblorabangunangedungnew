e9642da668c7c0aa86b83d0dd11158a3b86d2e29
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-15 23:02:19 +0700

    e

--- resources/views/backend/07_penilikbangunan/09_doklapprainspeksi.blade.php
+++ resources/views/backend/07_penilikbangunan/09_doklapprainspeksi.blade.php
@@ -194,12 +194,13 @@ th {
                         <button class="button-baru" type="button"
     onclick="location.href='/dokpenilikpracreate/{{ $data->id }}';"
     style="cursor: pointer; margin-left:10px; color:black;">
-    <i class="bi bi-file-earmark-plus" style="margin-right: 5px;"></i> Upload Dok Foto
+    <i class="bi bi-file-earmark-plus" style="margin-right: 5px;"></i> Simpan Data Pra Inspeksi
 </button>
 
-<a href="{{ url()->previous() }}" class="button-newvalidasi" style="cursor: pointer; color:black; margin-left:5px; display: inline-flex; align-items: center; text-decoration: none;">
+<a href="{{ url('/dataallpenilikbg') }}" class="button-newvalidasi" style="cursor: pointer; color:black; margin-left:5px; display: inline-flex; align-items: center; text-decoration: none;">
     <i class="bi bi-arrow-left" style="margin-right: 5px;"></i> Kembali
 </a>
+
         @endcanany
 
     </div>
