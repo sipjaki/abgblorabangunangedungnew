316496075ee6725a9fe9ebcb69e0804db6c45450
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-15 21:59:36 +0700

    e

--- resources/views/backend/07_penilikbangunan/08_showsuratpenilik.blade.php
+++ resources/views/backend/07_penilikbangunan/08_showsuratpenilik.blade.php
@@ -439,6 +439,12 @@ th {
        style="position: relative; height: 100px; opacity: 1; z-index: 1;">
 </div>
 
+    <p style="margin-top: 10px;">
+      <strong><u>MOHAMAD ARIF HIDAYAT, ST</u></strong><br>
+      Pembina IV/a<br>
+      NIP. 19710506 199403 1 011
+    </p>
+</div>
 </div>
 </body>
 </html>
