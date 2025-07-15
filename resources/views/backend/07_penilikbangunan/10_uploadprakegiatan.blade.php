6c7e8d26fc8a5ce4da53e691ace88adbf5621252
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-15 23:54:18 +0700

    e

--- resources/views/backend/07_penilikbangunan/10_uploadprakegiatan.blade.php
+++ resources/views/backend/07_penilikbangunan/10_uploadprakegiatan.blade.php
@@ -152,9 +152,8 @@ th {
 
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
-
-                        <button class="button-newvalidasi" type="button"
-    onclick="window.location.href='{{ url('dataallhibahbangunan') }}';"
+<button class="button-newvalidasi" type="button"
+    onclick="window.location.href='{{ url()->previous() }}';"
     style="cursor: pointer; margin-left:10px; color:black;">
     <i class="bi bi-arrow-left" style="margin-right: 5px;"></i> Kembali
 </button>
