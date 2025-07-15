b5c1c159dc80221ff502cacacf32b44c5fc4b820
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-15 15:59:07 +0700

    e

--- resources/views/backend/07_penilikbangunan/08_showsuratpenilik.blade.php
+++ resources/views/backend/07_penilikbangunan/08_showsuratpenilik.blade.php
@@ -416,7 +416,7 @@ th {
   </p>
 </div>
 
-<div class="ttd" style="font-size: 12px; margin-top: 30px; line-height: 1.6; margin-left: 200px; width: fit-content;">
+<div class="ttd" style="font-size: 12px; margin-top: 30px; line-height: 1.6; padding-left: 120px;">
   <p>
     Ditetapkan di : Blora<br>
     pada tanggal : {{ \Carbon\Carbon::parse($surat->tanggaltugas)->translatedFormat('d F Y') }}
