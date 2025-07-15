85a0b87832cb3bd78648fe181e8046917b7cb7c6
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-15 13:14:46 +0700

    e

--- resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
@@ -1172,6 +1172,24 @@
 </a>
 
     <ul class="nav nav-treeview" style="margin-left: 15px;">
+
+
+<div style="margin-bottom: 10px; margin-top: 10px;">
+    <p style="
+        font-size: 13px;
+        margin-left: 12px;
+        margin-bottom: -5px;
+        font-weight: bold;
+        color: #ffffff;
+        background-color: #0d6efd;
+        display: inline-block;
+        padding: 4px 12px;
+        border-radius: 8px;
+        box-shadow: 1px 1px 3px rgba(0,0,0,0.2);
+    ">
+        <i class="bi bi-grid-1x2-fill" style="margin-right: 6px;"></i> Main Menu
+    </p>
+</div>
     {{-- <li class="nav-item">
         <a href="/404" class="nav-link" style="display: flex; align-items: center;">
             <i class="bi bi-person-badge nav-icon" style="color: white; font-size: 16px; flex-shrink: 0;"></i>
@@ -1198,6 +1216,32 @@
             <p style="color: white; margin: 0 0 0 8px;">Data Inspeksi</p>
         </a>
     </li>
+
+
+<div style="margin-bottom: 10px; margin-top: 10px;">
+    <p style="
+        font-size: 13px;
+        margin-left: 12px;
+        margin-bottom: -5px;
+        font-weight: bold;
+        color: #ffffff;
+        background-color: #0d6efd;
+        display: inline-block;
+        padding: 4px 12px;
+        border-radius: 8px;
+        box-shadow: 1px 1px 3px rgba(0,0,0,0.2);
+    ">
+        <i class="bi bi-grid-1x2-fill" style="margin-right: 6px;"></i> Pengaturan Data
+    </p>
+</div>
+
+    <li class="nav-item">
+        <a href="/dataallpenilikbg" class="nav-link" style="display: flex; align-items: center;">
+            <i class="bi bi-folder2-open nav-icon" style="color: white; font-size: 16px; flex-shrink: 0;"></i>
+            <p style="color: white; margin: 0 0 0 8px;">Data Petugas</p>
+        </a>
+    </li>
+
 </ul>
 
 </li>
