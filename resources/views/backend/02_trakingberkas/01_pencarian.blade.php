10c53445c99e7482204867fa034d183742722885
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-16 18:28:12 +0700

    e

--- resources/views/backend/02_trakingberkas/01_pencarian.blade.php
+++ resources/views/backend/02_trakingberkas/01_pencarian.blade.php
@@ -1,3 +1,76 @@
+
+<!doctype html>
+<html lang="en">
+  <!--begin::Head-->
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Pencarian Berkas Permohonan PBG/SLF</title>
+    <!--begin::Primary Meta Tags-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="title" content="Sistem Informasi Pembina Jasa Konstruksi Kabupaten Blora Provinsi Jawa Tengah" />
+    <meta name="author" content="ColorlibHQ" />
+    <meta
+      name="description"
+      content="Sistem Informasi Pembina Jasa Konstruksi Kabupaten Blora Provinsi Jawa Tengah"
+    />
+    <meta
+      name="keywords"
+      content="Sistem Informasi Pembina Jasa Konstruksi Kabupaten Blora Provinsi Jawa Tengah"
+    />
+    <!--end::Primary Meta Tags-->
+    <!--begin::Fonts-->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css"
+      integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q="
+      crossorigin="anonymous"
+    />
+    <!--end::Fonts-->
+    <!--begin::Third Party Plugin(OverlayScrollbars)-->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/styles/overlayscrollbars.min.css"
+      integrity="sha256-tZHrRjVqNSRyWg2wbppGnT833E/Ys0DHWGwT04GiqQg="
+      crossorigin="anonymous"
+    />
+    <!--end::Third Party Plugin(OverlayScrollbars)-->
+    <!--begin::Third Party Plugin(Bootstrap Icons)-->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+      integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI="
+      crossorigin="anonymous"
+    />
+    <!--end::Third Party Plugin(Bootstrap Icons)-->
+    <!--begin::Required Plugin(AdminLTE)-->
+    <link rel="stylesheet" href="/assets/00_administrator/dist/css/adminlte.css" />
+    <!--end::Required Plugin(AdminLTE)-->
+    <!-- apexcharts -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.css"
+      integrity="sha256-4MX+61mt9NVvvuPjUWdUdyfZfxSB1/Rf9WtqRHgG5S0="
+      crossorigin="anonymous"
+    />
+    <!-- jsvectormap -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/css/jsvectormap.min.css"
+      integrity="sha256-+uGLJmmTKOqBr+2E6KDYs/NRsHxSkONXFHUL0fy2O/4="
+      crossorigin="anonymous"
+    />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
+
+    <link rel="icon" href="/assets/abgblora/logo/logokabupatenblora.png" type="image/x-icon">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+
+  </head>
+  <!--end::Head-->
+
+
 <style>
  body {
         font-family: 'Poppins', sans-serif;
@@ -44,7 +117,7 @@ th {
 
 </style>
 
-@include('backend.00_administrator.00_baganterpisah.01_header')
+{{-- @include('backend.00_administrator.00_baganterpisah.01_header') --}}
 
 <!--begin::Body-->
 <body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
@@ -159,7 +232,7 @@ th {
                     box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
                     width: 100%;
                 ">
-                <span style="font-family: 'Poppins', sans-serif;">📌 Halaman : {{$title}}</span>
+                <span style="font-family: 'Poppins', sans-serif;">📌 Halaman : Berkas Pencarian Permohonan PBG/SLF</span>
                 </div>
 
 <div class="container py-4">
@@ -171,7 +244,7 @@ th {
     </div>
 
     {{-- Form Pencarian --}}
-    <form method="GET" action="{{ route('betrackingdata') }}" class="row g-3 justify-content-center mb-4">
+    <form method="GET" action="{{ route('betracking') }}" class="row g-3 justify-content-center mb-4">
         <div class="col-md-6">
             <input type="text" name="noregissimbg" id="noregissimbg"
                    class="form-control @error('noregissimbg') is-invalid @enderror"
