13f52c390b4a759d203dffc588df723301a5146a
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-18 03:24:33 +0700

    e

--- resources/views/backend/04_bantuanteknis/05_datakonsultan/04_tambahdatapengkajiteknis.blade.php
+++ resources/views/backend/04_bantuanteknis/05_datakonsultan/04_tambahdatapengkajiteknis.blade.php
@@ -194,7 +194,7 @@ th {
         {{-- ======================================================= --}}
                     <div class="col-md-12">
                         <!--begin::Quick Example-->
-                  <form action="{{ route('create.bebantekkonsultannewjasa') }}" method="POST" enctype="multipart/form-data">
+                  <form action="{{ route('bepengkajiteknisnewcreate') }}" method="POST" enctype="multipart/form-data">
           @csrf
                             <!-- begin::Body -->
                             <div class="card-body">
