a61a50fe479b6bd5acd6286ccbe27414c4364eb1
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-20 03:04:59 +0700

    e

--- resources/views/frontend/android/02_pbg/01_fungsicampuran.blade.php
+++ resources/views/frontend/android/02_pbg/01_fungsicampuran.blade.php
@@ -36,31 +36,33 @@
 
         </div>
 
+@foreach ($data as $item)
 
-        <div class="flex flex-col space-y-3 px-[18px]">
-            <!-- Card 1 -->
-<a href="#" class="bg-white rounded-xl flex flex-col p-4 hover:shadow-md transition">
-  <!-- Gambar Thumbnail Penuh -->
-  <div class="w-full h-auto rounded-lg overflow-hidden">
-<div style="margin-top: 10px;">
-    @if($data->berkas && file_exists(public_path('storage/' . $data->berkas)))
-        <a href="{{ asset('storage/' . $data->berkas) }}" target="_blank" style="text-decoration: none; color: inherit;">
+<div class="flex flex-col space-y-3 px-[18px]">
+    <!-- Card 1 -->
+    <a href="#" class="bg-white rounded-xl flex flex-col p-4 hover:shadow-md transition">
+        <!-- Gambar Thumbnail Penuh -->
+        <div class="w-full h-auto rounded-lg overflow-hidden">
+            <div style="margin-top: 10px;">
+                @if($item->berkas && file_exists(public_path('storage/' . $item->berkas)))
+                <a href="{{ asset('storage/' . $item->berkas) }}" target="_blank" style="text-decoration: none; color: inherit;">
+                    <i class="bi bi-file-earmark-pdf-fill" style="font-size: 48px; color: #dc3545;"></i>
+                    <br>
+                    <span style="font-weight: 600; font-family: 'Poppins', sans-serif;">Berkas</span>
+        </a>
+        @elseif($item->berkas)
+        <a href="{{ asset($item->berkas) }}" target="_blank" style="text-decoration: none; color: inherit;">
             <i class="bi bi-file-earmark-pdf-fill" style="font-size: 48px; color: #dc3545;"></i>
             <br>
             <span style="font-weight: 600; font-family: 'Poppins', sans-serif;">Berkas</span>
         </a>
-    @elseif($data->berkas)
-        <a href="{{ asset($data->berkas) }}" target="_blank" style="text-decoration: none; color: inherit;">
-            <i class="bi bi-file-earmark-pdf-fill" style="font-size: 48px; color: #dc3545;"></i>
-            <br>
-            <span style="font-weight: 600; font-family: 'Poppins', sans-serif;">Berkas</span>
-        </a>
-    @else
+        @else
         <p style="font-family: 'Poppins', sans-serif; font-weight: 600;">Data belum diupdate</p>
-    @endif
-</div>
+        @endif
+    </div>
 
 </div>
+@endforeach
 
   <br>
   <!-- Info Teks -->
