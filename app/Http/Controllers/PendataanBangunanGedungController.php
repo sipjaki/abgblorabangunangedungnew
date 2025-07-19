917c258c9c2bb3b2b476ac4016daf2d0aed1cc9f
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-19 19:49:33 +0700

    e

--- app/Http/Controllers/PendataanBangunanGedungController.php
+++ app/Http/Controllers/PendataanBangunanGedungController.php
@@ -499,6 +499,7 @@ public function bedatabgprofiltanah($id)
 public function bedatabgprofiltanahupdate($id)
 {
     // Ambil data bantuan teknis berdasarkan ID
+    $databantuanteknis = databgkepemilikan::find($id);
     $databantuanteknis = databgtanah::find($id);
 
     if (!$databantuanteknis) {
