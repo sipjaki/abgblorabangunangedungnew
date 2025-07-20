dba1c812ec470a23ffc0f116d45e9468d74a748b
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-20 19:51:44 +0700

    e

--- resources/views/backend/01_pbgslf/00_informasi/04_updatehunian.blade.php
+++ resources/views/backend/01_pbgslf/00_informasi/04_updatehunian.blade.php
@@ -194,7 +194,7 @@ th {
         {{-- ======================================================= --}}
                     <div class="col-md-12">
                         <!--begin::Quick Example-->
-                  <form action="{{ route('updatefungsicampurannew', $data->id) }}" method="POST" enctype="multipart/form-data">
+                  <form action="{{ route('bepbghunianupdatenew', $data->id) }}" method="POST" enctype="multipart/form-data">
           @csrf
                             <!-- begin::Body -->
                             <div class="card-body">
