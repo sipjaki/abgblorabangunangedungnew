e0e1315198e45d30e14afdf852bb2fe248f415a4
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-21 03:59:34 +0700

    e

--- resources/views/backend/06_krk/01_pengesahanusaha/09_uploadsuratmanual.blade.php
+++ resources/views/backend/06_krk/01_pengesahanusaha/09_uploadsuratmanual.blade.php
@@ -247,6 +247,7 @@ onchange="previewPDF(event)" />
         style="width: 100%; height: 400px; border: 1px solid #ccc; border-radius: 6px;"
       ></iframe>
 
+      <br>
       <!-- Tombol Download -->
       <a
         href="{{ asset($data->suratuploadmanual) }}"
