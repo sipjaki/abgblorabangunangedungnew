36a47d8688627239851e88225510d31f7e6ec61e
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-21 02:39:48 +0700

    e

--- resources/views/frontend/abgblora/06_permohonankrk/02_permohonankrkpemohon/01_krkusahapercobaan.blade.php
+++ resources/views/frontend/abgblora/06_permohonankrk/02_permohonankrkpemohon/01_krkusahapercobaan.blade.php
@@ -814,10 +814,12 @@ document.addEventListener('DOMContentLoaded', function () {
         <!-- Text Label dan Link Download -->
         <div class="flex flex-col text-sm">
             <span>Upload Surat Permohonan KRK | 10 MB </span>
-            <a href="/asset/abgblora/logo/Surat_Permohonan_KRK.docx" download
-                class="text-blue-600 underline hover:text-blue-800 mt-1" style="color: red">
-                Download Contoh Surat Permohonan (.docx)
-            </a>
+          <a href="/asset/abgblora/logo/Surat_Permohonan_KRK.docx" download
+   class="mt-1 underline hover:text-black"
+   style="color: red;">
+   Download Contoh Surat Permohonan (.docx)
+</a>
+
         </div>
     </label>
 
