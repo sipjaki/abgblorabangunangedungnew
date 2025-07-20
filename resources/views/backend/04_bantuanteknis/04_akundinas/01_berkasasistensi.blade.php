a52596f1b8436baef008d85a328c0785367f6955
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-21 02:12:43 +0700

    e

--- resources/views/backend/04_bantuanteknis/04_akundinas/01_berkasasistensi.blade.php
+++ resources/views/backend/04_bantuanteknis/04_akundinas/01_berkasasistensi.blade.php
@@ -277,7 +277,7 @@ th {
                                   <tr>
         <th style="background-color: #ADD8E6;">No</th>
         <th style="background-color: #ADD8E6;"><i class="fas fa-user"></i> Jenis Pengajuan</th>
-        <th style="background-color: #ADD8E6;"><i class="fas fa-user-tie"></i> Pemohon</th>
+        <th style="background-color: #ADD8E6;"><i class="fas fa-user-tie"></i> Dinas</th>
         <th style="background-color: #ADD8E6;"><i class="fas fa-phone"></i> Telepon</th>
         <th style="background-color: #ADD8E6;"><i class="fas fa-envelope"></i> No Surat</th>
         <th style="background-color: #ADD8E6;"><i class="fas fa-calendar"></i> Tanggal Surat</th>
@@ -317,7 +317,7 @@ th {
                                 <tr class="align-middle">
                                  <td>{{ $loop->iteration }}</td>
             <td>{{ optional($item->jenispengajuanbantek)->jenispengajuan ?? '-' }}</td>
-            <td>{{ $item->nama_pemohon ?? '-' }}</td>
+            <td>{{ $item->dinas->user->name ?? '-' }}</td>
             <td>{{ $item->no_telepon ?? '-' }}</td>
             <td>{{ $item->nosurat ?? '-' }}</td>
             <td>{{ \Carbon\Carbon::parse($item->tanggalsurat)->format('d-m-Y') }}</td>
