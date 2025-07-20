d428a812f1d1e93c9a13073652bde7a59611fda0
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-20 19:52:13 +0700

    e

--- resources/views/backend/01_pbgslf/00_informasi/04_updatehunian.blade.php
+++ resources/views/backend/01_pbgslf/00_informasi/04_updatehunian.blade.php
@@ -259,7 +259,7 @@ th {
     <hr class="my-4" style="border-top: 2px dashed #0d6efd; width: 60%; margin: auto;">
     <h5 style="color: #0d6efd; font-weight: bold; margin-top: 5px; font-size:16px;">
         <i class="bi bi-upload" style="margin-right: 6px;"></i>
-        Upload Informasi PBG Fungsi Campuran
+        Upload Informasi PBG Fungsi Hunian
     </h5>
     <hr class="my-4" style="border-top: 2px dashed #0d6efd; width: 60%; margin: auto;">
 </div>
