add6fd9ec12c2274f9627350b93a54a4c2e82d13
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-20 23:12:10 +0700

    e

--- resources/views/frontend/android/00_fiturmenu/04_menunavigasi.blade.php
+++ resources/views/frontend/android/00_fiturmenu/04_menunavigasi.blade.php
@@ -25,7 +25,7 @@
           <p class="font-medium text-sm text-[#757C98] leading-[21px]">Tracking</p>
         </div>
       </a>
-      <a href="/resbgindex">
+      <a href="/bependataanbangunangedung">
         <div class="flex flex-col items-center gap-2">
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/android/menunavigasi/03.png" class="w-full h-full object-cover" alt="icon" loading="lazy">
