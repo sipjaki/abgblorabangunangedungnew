d4dc86ad67906e3df4a08d650374e62c96c30327
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-22 03:57:41 +0700

    e

--- resources/views/backend/06_krk/02_berkaspermohonan/hunian.blade.php
+++ resources/views/backend/06_krk/02_berkaspermohonan/hunian.blade.php
@@ -655,10 +655,7 @@ th {
 
 @if($subdata->where('krkhunian_id', $item->id)->count() > 0)
     <a href="{{ route('berkas.perpengesahanhunianber', $item->id) }}"
-        class="button-lolos"
-        style="text-decoration: none; border-radius: 15px; padding: 8px 16px; background-color: #10B981; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
-        onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#10B981'; this.style.border='1px solid #10B981';"
-        onmouseout="this.style.backgroundColor='#10B981'; this.style.color='white'; this.style.border='none';">
+        class="button-hijau">
         <i class="bi bi-folder" style="margin-right: 5px;"></i> Lihat Dok Pengesahan
     </a>
 @else
