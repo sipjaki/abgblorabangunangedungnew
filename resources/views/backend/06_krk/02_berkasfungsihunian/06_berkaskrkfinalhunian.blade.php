fdf00298131fa31d2f768486bb543fc1ff1341c1
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-22 04:56:22 +0700

    e

--- resources/views/backend/06_krk/02_berkasfungsihunian/06_berkaskrkfinalhunian.blade.php
+++ resources/views/backend/06_krk/02_berkasfungsihunian/06_berkaskrkfinalhunian.blade.php
@@ -94,7 +94,7 @@
 
                         @canany(['superadmin', 'admin'])
                             <div style="display: flex; justify-content: flex-end; margin-bottom:5px;">
-                                <button class="button-newvalidasi" type="button" onclick="location.href='{{ route('krkhunianindex') }}';" style="cursor: pointer; color:white;">
+                                <button class="button-newvalidasi" type="button" onclick="location.href='{{ route('bekrkhunianindexgit ') }}';" style="cursor: pointer; color:white;">
                                     <i class="bi bi-arrow-left" style="margin-right: 5px;"></i> Kembali
                                 </button>
                             </div>
