89cd981f8e09e0d780c2393f97c45f0535497a2a
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-22 04:51:56 +0700

    e

--- resources/views/backend/06_krk/01_pengesahanusaha/08_ttdmanual.blade.php
+++ resources/views/backend/06_krk/01_pengesahanusaha/08_ttdmanual.blade.php
@@ -284,7 +284,7 @@
                                                                     <td style="text-align: center;">8</td>
                                                                     <td style="text-align: left;">(KDB) Koefisien Dasar Bangunan</td>
                                                                     <td style="text-align: center;">:</td>
-                                                                    <td style="text-align: left;">{{ $item->klb ?? 'Belum Dibuatkan' }}%</td>
+                                                                    <td style="text-align: left;">{{ $item->klb ?? 'Belum Dibuatkan' }}</td>
                                                                 </tr>
                                                                 <tr>
                                                                     <td style="text-align: center;">10</td>
