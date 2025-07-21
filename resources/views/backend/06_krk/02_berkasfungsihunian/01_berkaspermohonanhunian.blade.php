f3f633f57e74504462aa0bbd5c86515636644bf6
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-22 02:50:56 +0700

    e

--- resources/views/backend/06_krk/02_berkasfungsihunian/01_berkaspermohonanhunian.blade.php
+++ resources/views/backend/06_krk/02_berkasfungsihunian/01_berkaspermohonanhunian.blade.php
@@ -172,21 +172,27 @@ th {
 
 @endcanany --}}
 
+ @canany(['pemohon'])
+    <div style="display: flex; justify-content: flex-end; margin-bottom:5px;">
+        <button class="button-newkembali"
+                type="button"
+                onclick="location.href='{{ route('bekrkhunianpemohon.indexpemohon') }}';"
+                style="cursor: pointer; color:black;">
+            <i class="bi bi-arrow-left" style="margin-right: 5px;"></i> Kembali
+        </button>
+    </div>
+@endcanany
 
-@can('pemohon')
+
+{{-- @can('pemohon')
 
            <a href="/bekrkhunianpemohon">
     <button class="button-newvalidasi">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-    viewBox="0 0 16 16">
-    <path fill-rule="evenodd"
-      d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z" />
-  </svg>
   Kembali
 </button>
 
 </a>
-@endcan
+@endcan --}}
 
 @can('lsppenerbit')
    <button
