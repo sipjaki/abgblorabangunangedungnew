4eb958b76ac05cfc8f035dc7c83e7c57e6547dcb
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-22 17:25:40 +0700

    e

--- resources/views/backend/06_krk/02_berkaspermohonan/keagamaan.blade.php
+++ resources/views/backend/06_krk/02_berkaspermohonan/keagamaan.blade.php
@@ -628,39 +628,22 @@ th {
 @endif
         @if (!$item->is_validated)
     <!-- BELUM DIVALIDASI, tombol nonaktif merah -->
-    <button class="button-dikembalikan"
-        disabled
-        style="background-color: #dc3545; color: white; border: none; padding: 10px 25px;
-               border-radius: 15px; font-size: 14px; cursor: not-allowed;
-               display: flex; align-items: center; justify-content: center;
-               opacity: 0.6;"
-        title="Silakan validasi terlebih dahulu"
-    >
+    <button class="button-merah"
+        disabled>
         <i class="bi bi-pencil-fill" style="margin-right: 5px;"></i> Manual
     </button>
 @else
     @if($subdata->where('krkkeagamaan_id', $item->id)->count() > 0)
         <!-- SUDAH DIVALIDASI tapi data pengesahan sudah ada, tombol hijau tapi nonaktif -->
-        <button class="button-download"
+        <button class="button-merah"
             disabled
-            style="border: none; padding: 10px 25px;
-                   border-radius: 15px; font-size: 14px; cursor: not-allowed;
-                   display: flex; align-items: center; justify-content: center;
-                   opacity: 0.7;"
-            title="Dokumen pengesahan sudah ada"
-        >
+                        title="Dokumen pengesahan sudah ada">
             <i class="bi bi-pencil-fill" style="margin-right: 5px;"></i> Manual
         </button>
     @else
         <!-- SUDAH DIVALIDASI dan data pengesahan belum ada, tombol aktif dan bisa diklik -->
         <a href="{{ route('permohonan.perpengesahanagama', $item->id) }}" style="text-decoration: none;">
-            <button class="button-lolos"
-                onmouseover="this.style.backgroundColor='#D1FAE5'; this.style.color='black';"
-                onmouseout="this.style.backgroundColor='#28a745'; this.style.color='white';"
-                style="background-color:#28a745; color: white; border: none; padding: 10px 25px;
-                       border-radius: 15px; font-size: 14px; cursor: pointer;
-                       display: flex; align-items: center; justify-content: center;
-                       transition: background-color 0.3s, color 0.3s;">
+            <button class="button-hijau">
                 <i class="bi bi-pencil-fill" style="margin-right: 8px;"></i> Manual
             </button>
         </a>
