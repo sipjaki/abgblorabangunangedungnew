af5425c47f24bfb1e03c0e9385ecaa23fcaac7f4
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-22 17:07:32 +0700

    e

--- resources/views/backend/06_krk/01_pengesahanusaha/09_uploadsuratmanual.blade.php
+++ resources/views/backend/06_krk/01_pengesahanusaha/09_uploadsuratmanual.blade.php
@@ -282,13 +282,13 @@ onchange="previewPDF(event)" />
         <i class="bi bi-download mr-2"></i> Download Berkas KRK
       </a>
     </div>
-      @canany(['superadmin', 'admin'])
+      {{-- @canany(['superadmin', 'admin'])
   Belum Upload Berkas, Silahkan Upload Dokumen Final KRK.
   @endcanany
 
   @canany(['pemohon'])
   Belum Ada Dokumen KRK Saudara, Silahkan Menunggu DPUPR Kabupaten Blora.
-  @endcanany
+  @endcanany --}}
 
 </div>
     </div>
