d4b201334e04fc8d577091fdebcb1d5c143913af
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-23 00:58:06 +0700

    e

--- resources/views/backend/06_krk/02_berkaspermohonan/menaratelkom.blade.php
+++ resources/views/backend/06_krk/02_berkaspermohonan/menaratelkom.blade.php
@@ -771,41 +771,6 @@ th {
 <td style="text-align: center; vertical-align: middle; width: 100%;">
     <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; gap: 10px;">
 
-
-@if($subdata->where('krkusaha_id', $item->id)->count() > 0)
-    <a href="{{ route('permohonan.permohonankrkusahafinal', $item->id) }}"
-        class="button-hijau"
-        style="text-decoration: none; border-radius: 15px; padding: 8px 16px; background-color: #10B981; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
-        onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#10B981'; this.style.border='1px solid #10B981';"
-        onmouseout="this.style.backgroundColor='#10B981'; this.style.color='white'; this.style.border='none';">
-        <i class="bi bi-folder" style="margin-right: 5px;"></i> Dok TTD Otomatis
-    </a>
-@else
-    <button
-        class="button-merah"
-        style="cursor: not-allowed; border-radius: 15px; padding: 8px 16px; background-color: #DC3545; color: white; border: none;"
-        disabled>
-        <i class="bi bi-folder-x" style="margin-right: 5px;"></i> Berkas Otomatis
-    </button>
-@endif
-
-@if($subdata->where('krkusaha_id', $item->id)->count() > 0)
-    <a href="{{ route('permohonankrkusahafinalmanual', $item->id) }}"
-        class="button-hijau"
-        style="text-decoration: none; border-radius: 15px; padding: 8px 16px; background-color: #10B981; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
-        onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#10B981'; this.style.border='1px solid #10B981';"
-        onmouseout="this.style.backgroundColor='#10B981'; this.style.color='white'; this.style.border='none';">
-        <i class="bi bi-folder" style="margin-right: 5px;"></i> Dok TTD Manual
-    </a>
-@else
-    <button
-        class="button-merah"
-        style="cursor: not-allowed; border-radius: 15px; padding: 8px 16px; background-color: #DC3545; color: white; border: none;"
-        disabled>
-        <i class="bi bi-folder-x" style="margin-right: 5px;"></i> Berkas Manual
-    </button>
-@endif
-
                 <a href="{{ route('dokuploadkrkusaha', $item->id) }}"
                     class="button-baru">
                 <i class="bi bi-folder" style="margin-right: 5px;"></i> Upload Berkas Final
