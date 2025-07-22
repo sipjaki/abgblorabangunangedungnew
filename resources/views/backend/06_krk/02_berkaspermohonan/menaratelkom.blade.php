41bd2750eaec1d2c9bf9fb78d6a5359b0187b668
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-23 00:45:14 +0700

    e

--- resources/views/backend/06_krk/02_berkaspermohonan/menaratelkom.blade.php
+++ resources/views/backend/06_krk/02_berkaspermohonan/menaratelkom.blade.php
@@ -221,10 +221,10 @@ th {
                                     <th style="background-color: #ADD8E6;"><i class="fas fa-drafting-compass"></i> Site Plan</th>
                                     <th style="background-color: #ADD8E6;"><i class="fas fa-pen-fancy"></i> Tanda Tangan</th> --}}
                                     <th style="background-color: #ADD8E6;"><i class="fas fa-tasks"></i> Verifikasi DPUPR</th>
-                                    <th style="background-color: #ADD8E6;"><i class="fas fa-tasks"></i> Dok Lapangan</th>
+                                    {{-- <th style="background-color: #ADD8E6;"><i class="fas fa-tasks"></i> Dok Lapangan</th> --}}
                                     <th style="background-color: #ADD8E6;"><i class="fas fa-tasks"></i> Status Cek Lapangan</th>
-                                    <th style="background-color: #ADD8E6;"><i class="fas fa-stamp"></i> Olah Data KRK</th>
-                                    <th style="background-color: #ADD8E6;"><i class="fas fa-stamp"></i> Buat Data KRK</th>
+                                    {{-- <th style="background-color: #ADD8E6;"><i class="fas fa-stamp"></i> Olah Data KRK</th> --}}
+                                    {{-- <th style="background-color: #ADD8E6;"><i class="fas fa-stamp"></i> Buat Data KRK</th> --}}
                              <th style="background-color: #ADD8E6;">
     <i class="fas fa-database" style="margin-right: 6px;"></i> Status Olah Data
 </th>
@@ -296,7 +296,7 @@ th {
 
 
 <!-- Tombol Validasi -->
-<td style="text-align: center; display: flex; justify-content: center; align-items: center; height: 60px;">
+{{-- <td style="text-align: center; display: flex; justify-content: center; align-items: center; height: 60px;">
   @if($item->verifikasi1 == 'lolos')
     <button
         class="button-hijau"
@@ -315,7 +315,7 @@ th {
             <i class="bi bi-patch-check" style="margin-right: 5px;"></i> Validasi
         </button>
     @endif
-</td>
+</td> --}}
 
 <!-- Modal Konfirmasi -->
 <div id="confirmModal" style="display: none; position: fixed; inset: 0; background-color: rgba(0,0,0,0.5); z-index: 1000; justify-content: center; align-items: center;">
@@ -380,7 +380,7 @@ th {
     }
 </script>
 
-  <td style="text-align: center;">
+  {{-- <td style="text-align: center;">
                 <a href="{{ route('doklapkrkusaha.show', $item->id) }}"
                     class="button-baru">
                 <i class="bi bi-folder" style="margin-right: 5px;"></i> Lihat Dok Lapangan
@@ -407,7 +407,7 @@ th {
         <i class="bi bi-patch-check" style="margin-right: 5px;"></i> Verifikasi
     </button>
   @endif
-</td>
+</td> --}}
 
 <!-- Modal Verifikasi2 -->
 <div id="confirmModalVerifikasi2" style="display: none; position: fixed; inset: 0; background-color: rgba(0,0,0,0.5); z-index: 1001; justify-content: center; align-items: center;">
@@ -477,7 +477,7 @@ th {
 
 
 
-                            <td>
+                            {{-- <td>
                                   <div style="display: flex; flex-direction: column; align-items: center;">
                                 @if (!$item->is_validated)
                                 <!-- Tombol Triger Modal -->
@@ -496,7 +496,7 @@ th {
                                 </button>
                                 @endif
                                 </div>
-                            </td>
+                            </td> --}}
 
                             <!-- Modal Validasi -->
 <!-- Modal Validasi -->
@@ -585,7 +585,7 @@ th {
 <td style="text-align: center; vertical-align: middle; width: 100%;">
 
     <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; gap: 10px;">
-
+{{--
         @if (!$item->is_validated)
     <!-- BELUM DIVALIDASI, tombol nonaktif merah -->
     <button class="button-merah"
@@ -660,7 +660,7 @@ th {
             </button>
         </a>
     @endif
-@endif
+@endif --}}
 
 
 @if($subdata->where('krkusaha_id', $item->id)->count() > 0)
