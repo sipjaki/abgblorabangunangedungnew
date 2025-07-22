26619ed5e2d1f6c4f4adf163ce842bb92e3f2d43
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-22 16:42:46 +0700

    e

--- resources/views/backend/06_krk/02_berkasfungsihunian/06_berkaskrkfinalhunian.blade.php
+++ resources/views/backend/06_krk/02_berkasfungsihunian/06_berkaskrkfinalhunian.blade.php
@@ -356,7 +356,7 @@
                                                     DINAS PEKERJAAN UMUM DAN PENATAAN RUANG<br>
                                                     KABUPATEN BLORA<br><br>
 
-                                                    {{-- <div style="position: relative; width: 220px; height: 100px; margin-top:-15px;">
+                                                    <div style="position: relative; width: 220px; height: 100px; margin-top:-15px;">
                                                         <!-- TTD Kabupaten Blora agak ke kanan -->
                                                         <img src="/assets/abgblora/logo/ttdkabblora.png" alt=""
                                                              style="position: absolute; left: 10px; top: 0; height: 90px; z-index: 1;">
@@ -364,8 +364,9 @@
                                                         <!-- TTD PA Huda di kanan -->
                                                         <img src="/assets/abgblora/logo/ttdpahuda.png" alt=""
                                                              style="position: absolute; right: 0; top: 0; height: 80px; z-index: 2;">
-                                                    </div> --}}
-                                                    <br><br><br><br><br>
+                                                    </div>
+
+                                                    <br>
                                                     <div style="display: inline-flex; flex-direction: column; gap: 0;">
                                                         <strong style="margin-top: -25px; text-decoration: underline; line-height: 1;">
                                                             NIDZAMUDIN AL HUDA, ST
