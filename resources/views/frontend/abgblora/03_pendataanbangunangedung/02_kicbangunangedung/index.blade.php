3316f6930dbe0117a44a7efdcfb60844a9bffe17
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-27 06:46:20 +0700

    e

--- resources/views/frontend/abgblora/03_pendataanbangunangedung/02_kicbangunangedung/index.blade.php
+++ resources/views/frontend/abgblora/03_pendataanbangunangedung/02_kicbangunangedung/index.blade.php
@@ -274,6 +274,7 @@
                         <th style="text-align: center;">Kode Lokasi</th>
                         <th style="text-align: center;">Bidang</th>
                         <th style="text-align: center;">Sub Bidang</th>
+                        <th style="text-align: center;">Jumlah Bangunan</th>
                         <th style="text-align: center;">View</th>
                     </tr>
                 </thead>
@@ -302,10 +303,15 @@
         <span class="button-berkas">Data Belum Di Update</span>
     @endif
 </td>
+<td>
+    <button class="button-hijau">
+        {{ $item->kicdokumen->count() }}
+    </button>
+</td>
 
                         <td style="text-align: center;">
                             <a class="button-baru" href="/pendataankicbangunangedungshow/{{ $item->id }}" style="font-size: 12px !important;">
-                                  {{ $item->kicdokumen->count() }} Lihat
+                                 Lihat
                             </a>
                         </td>
                     </tr>
