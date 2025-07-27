edf976506d12e17d0fd97d4837e748abff7f2b0c
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-27 19:35:55 +0700

    e

--- resources/views/frontend/abgblora/08_bantuangambar/01_informasibantuangambar/01_informasibantuangambar.blade.php
+++ resources/views/frontend/abgblora/08_bantuangambar/01_informasibantuangambar/01_informasibantuangambar.blade.php
@@ -471,10 +471,10 @@
                         </div>
                     </div>
                     <div class="action-buttons">
-                        <a href="/assets/abgblora/logo/Surat_Permohonan_Bantuan_Gambar.docx" download class="btn-download inline-flex items-center gap-2 px-4 py-2 bg-blue-600 text-black font-semibold rounded hover:bg-blue-700">
+                        <a class="button-baru" href="/assets/abgblora/logo/Surat_Permohonan_Bantuan_Gambar.docx" download class="btn-download inline-flex items-center gap-2 px-4 py-2 bg-blue-600 text-black font-semibold rounded hover:bg-blue-700">
                             <i class="bi bi-download"></i> Unduh Formulir
                         </a>
-                        <a href="">
+                        <a class="button-berkas" href="">
                             <button class="btn-register"><i class="bi bi-pencil-square"></i> Ajukan Permohonan !</button>
                         </a>
                     </div>
