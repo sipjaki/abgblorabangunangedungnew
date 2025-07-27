379da4b467af25db8b08c285bd416a23d25c4329
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-27 21:42:24 +0700

    e

--- resources/views/backend/00_administrator/00_baganterpisah/02_footer.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/02_footer.blade.php
@@ -5,7 +5,7 @@
     <!--end::To the end-->
     <!--begin::Copyright-->
     <strong>
-        <img src="/assets/icon/logokabupatenblora.png" alt="" width="20">
+        <img src="/assets/abgblora/logo/logokabupatenblora.png" alt="" width="20">
         <img src="/assets/icon/pupr.png" alt="" width="20">
         &copy; Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Blora Provinsi Jawa Tengah &nbsp; 2025 | Built and Deployed By
         <a href="https://wa.me/6281321455855" target="_blank" class="text-decoration-none" style="color: green"> CV. Angkasa Raya Teknik</a>.
