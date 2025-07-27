fb2dcf3ab55a8b6d3b54df2c46ba3e36eaa79339
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-27 07:24:56 +0700

    e

--- resources/views/frontend/abgblora/06_permohonankrk/02_permohonankrkpemohon/00_index.blade.php
+++ resources/views/frontend/abgblora/06_permohonankrk/02_permohonankrkpemohon/00_index.blade.php
@@ -99,7 +99,7 @@
   ">
 
     <section id="breadcrumb" class="container max-w-[1130px] mx-auto" style="margin-top: 150px;">
-        <br><br>
+        {{-- <br><br>
         <div class="flex items-center gap-[20px]">
           <!-- Gambar di kiri -->
           <img src="/assets/abgblora/logo/iconabgblora.png" alt="" class="w-[60px] -my-[15px]" width="10%" style="margin-right: 20px;">
@@ -110,16 +110,12 @@
              {{$title}}
             </a>
           </div>
-        </div>
+        </div> --}}
 
       </section>
 
 
 
-    <section id="header" class="container max-w-[1130px] mx-auto flex flex-col sm:flex-row items-center justify-between gap-2" style="margin-top:10px;">
-
-
-      </section>
 
 
      <section id="details" class="container max-w-[1130px] mx-auto flex flex-col sm:flex-row gap-5">
