22834ebd36b74eb5f389fb8b6f36c21ad6e12437
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-31 06:50:39 +0700

    e

--- resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/03_datatanahbangunan/02_tambahdatatanah.blade.php
+++ resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/03_datatanahbangunan/02_tambahdatatanah.blade.php
@@ -502,7 +502,7 @@ th {
 
         {{-- Tombol Submit --}}
         <div class="col-12 text-end mt-3">
-            <button type="button" class="button-baru" onclick="openModal()">
+            <button type="button" class="button-hijau" onclick="openModal()">
                 <i class="bi bi-save me-1"></i> Simpan Data Tanah
             </button>
         </div>
