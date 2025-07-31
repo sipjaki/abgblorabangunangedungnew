a2dcb4965d13da5fa12a0808c20068f90a4d589b
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-31 11:44:17 +0700

    e

--- resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/07_dokumenmep/02_tambahdatamep.blade.php
+++ resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/07_dokumenmep/02_tambahdatamep.blade.php
@@ -416,7 +416,7 @@ th {
         $catatanKey = 'catatanberkas' . $i;
     @endphp
 
-    <div class="col-md-4 mb-3">
+    <div class="col-md-6 mb-3">
         <label class="form-label d-block" style="color: black; font-weight: 600;">
             <i class="bi bi-file-earmark-text me-1" style="color: blue;"></i> {{ $judul }}
         </label>
