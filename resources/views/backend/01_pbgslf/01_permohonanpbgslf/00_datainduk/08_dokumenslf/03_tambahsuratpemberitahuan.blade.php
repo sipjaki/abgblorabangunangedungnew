c30b2180e5111473f518646dc79568a0a2486372
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-31 12:21:38 +0700

    e

--- resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/08_dokumenslf/03_tambahsuratpemberitahuan.blade.php
+++ resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/08_dokumenslf/03_tambahsuratpemberitahuan.blade.php
@@ -471,7 +471,7 @@ th {
     </div>
 
 <div class="text-end mt-4">
-    <button type="button" class="button-baru" onclick="openModal()">
+    <button type="button" class="button-hijau" onclick="openModal()">
         <i class="bi bi-save me-1"></i> Buat Surat Pemberitahuan
     </button>
 </div>
