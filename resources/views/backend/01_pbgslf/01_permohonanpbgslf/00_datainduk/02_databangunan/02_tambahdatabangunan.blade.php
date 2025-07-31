af2d5c0f40c9cf4a61ddb923775442b9e2981dd0
SipjakiBlora <sipjakiblora@gmail.com>
2025-07-31 13:30:31 +0700

    erg

--- resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/02_databangunan/02_tambahdatabangunan.blade.php
+++ resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/02_databangunan/02_tambahdatabangunan.blade.php
@@ -503,7 +503,7 @@ th {
 
 <div class="col-md-6">
     <label class="form-label"><i class="bi bi-arrow-up-short text-primary me-1"></i> Tinggi Bangunan (meter)</label>
-    <input type="text" name="tinggibangunan" class="form-control @error('tinggibangunan') is-invalid @enderror" value="{{ old('tinggibangunan') }}">
+    <input type="number" name="tinggibangunan" class="form-control @error('tinggibangunan') is-invalid @enderror" value="{{ old('tinggibangunan') }}">
     @error('tinggibangunan')<div class="invalid-feedback">{{ $message }}</div>@enderror
 </div>
 <div class="col-md-6">
