52d3b790d44c405952381062e6b84f3ac503fdc1
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-01 14:35:32 +0700

    e

--- resources/views/frontend/abgblora/04_bantuanteknis/01_bantuanteknisindex.blade.php
+++ resources/views/frontend/abgblora/04_bantuanteknis/01_bantuanteknisindex.blade.php
@@ -248,7 +248,7 @@
             <option value="">-- Pilih Konsultan Asistensi --</option>
             @foreach ($datakonsultanbantek as $admin)
                 <option value="{{ $admin->id }}" {{ old('bujkkonsultan_id') == $admin->id ? 'selected' : '' }}>
-                    {{ $admin->namalengkap }}
+                    {{ $admin->name }}
                 </option>
             @endforeach
         </select>
