ba049c919c89bb566797a2b61fa59a3d64b49708
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-01 09:52:56 +0700

    e

--- resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/12_suratundangan/05_showundangantpa.blade.php
+++ resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/12_suratundangan/05_showundangantpa.blade.php
@@ -324,19 +324,25 @@
     <p style="text-align: right; margin-top: 20px; font-size:12px; font-family: 'Times New Roman', serif !important; font-size:14px;">Blora, {{ $surat ? \Carbon\Carbon::parse($surat->tanggalundangan)->translatedFormat('d F Y') : '-' }}</p>
 
     <div style="font-size: 12px; font-family: 'Times New Roman', serif !important;">
-      <table style="font-size: 14px; font-family: 'Times New Roman', serif !important; line-height: 1.8;">
+    <table style="line-height: 1.8;">
   <tr>
-    <td style="width: 100px;"><strong style="font-size: 14px; font-family: 'Times New Roman', serif !important;">Nomor</strong></td>
-    <td style="width: 10px;" style="font-size: 14px; font-family: 'Times New Roman', serif !important;">:</td>
-    <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;">050 / UND-{{ $surat->pbgslfbangunan->noregissimbg ?? '-' }}/{{ $surat->konsultasike ?? '-' }}/2025</td>
+    <td style="width: 100px; font-size: 14px; font-family: 'Times New Roman', serif !important;"><strong style="font-size: 14px; font-family: 'Times New Roman', serif !important;">Nomor</strong></td>
+    <td style="width: 10px; font-size: 14px; font-family: 'Times New Roman', serif !important;">:</td>
+    <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;">
+      050 / UND-{{ $surat->pbgslfbangunan->noregissimbg ?? '-' }}/{{ $surat->konsultasike ?? '-' }}/2025
+    </td>
   </tr>
   <tr>
-    <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;"><strong>Lampiran</strong></td>
+    <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;">
+      <strong style="font-size: 14px; font-family: 'Times New Roman', serif !important;">Lampiran</strong>
+    </td>
     <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;">:</td>
     <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;">-</td>
   </tr>
   <tr>
-    <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;"><strong>Perihal</strong></td>
+    <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;">
+      <strong style="font-size: 14px; font-family: 'Times New Roman', serif !important;">Perihal</strong>
+    </td>
     <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;">:</td>
     <td style="font-size: 14px; font-family: 'Times New Roman', serif !important;">Undangan Konsultasi</td>
   </tr>
