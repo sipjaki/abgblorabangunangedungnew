b09013f5a4681f251c3e7ea17cc1a531de21a4c7
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-02 01:24:56 +0700

    e

--- app/Http/Controllers/GambarbantuanController.php
+++ app/Http/Controllers/GambarbantuanController.php
@@ -746,16 +746,16 @@ public function feformbantuangambarcreate(Request $request)
     return redirect('/dashboard');
 }
 
-
 public function bebantuangambarpemohon(Request $request)
 {
     $user = Auth::user();
     $search = $request->input('search');
     $perPage = $request->input('perPage', 15);
 
-    $query = gambarbantuan::query();
+    // Hanya ambil data milik user yang login
+    $query = gambarbantuan::where('user_id', $user->id);
 
-    // Filter pencarian berdasarkan kolom sesuai yang kamu berikan
+    // Filter pencarian
     if ($search) {
         $query->where(function ($q) use ($search) {
             $q->where('namapemohon', 'like', "%{$search}%")
