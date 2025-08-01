292ec70140ac2acaf18dd27b6ee59b0906f6795c
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-02 00:11:51 +0700

    e

--- resources/views/backend/04_bantuanteknis/03_akunpemohonbantek/00_perbaikan/06_perbaikandata.blade.php
+++ resources/views/backend/04_bantuanteknis/03_akunpemohonbantek/00_perbaikan/06_perbaikandata.blade.php
@@ -97,7 +97,7 @@ th {
 
      <div class="container-fluid">
          <!--begin::Row-->
-         <div class="row" style="margin-right: 10px; margin-left:10px;">
+         <div class="putih row" style="margin-right: 10px; margin-left:10px;">
              <!-- /.card -->
              <div class="card mb-4">
                  {{-- <div class="card-header">
@@ -153,7 +153,7 @@ th {
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
 
-<button class="button-kembali" type="button"
+<button class="button-newvalidasi" type="button"
     onclick="window.location.href='{{ url()->previous() }}';"
     style="cursor: pointer; margin-left:10px; color:black;">
     <i class="bi bi-arrow-left" style="margin-right: 5px;"></i> Kembali
@@ -427,7 +427,7 @@ function previewPDF(event, previewContainerId, iframeId, messageId) {
 
                             <div style="display: flex; justify-content: flex-end; margin-bottom:20px;">
                                 <div class="flex justify-end">
-                               <button class="button-create" type="button" onclick="openModal()">
+                               <button class="button-hijau" type="button" onclick="openModal()">
                                     <i class="bi bi-save" style="margin-right: 5px;"></i>
                                     <span style="font-family: 'Poppins', sans-serif;">Simpan</span>
                                     </button>
