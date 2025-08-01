009bf30fbbd7aa6d772e469479647ce880bd107a
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-01 21:53:35 +0700

    e

--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/403.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/403.blade.php
@@ -8,7 +8,7 @@
 @extends('errors::minimal')
 
 @section('title', __('Akses Ditolak'))
-@section('code', '403')
+{{-- @section('code', '403') --}}
 @section('message')
     <div style="text-align: center; margin-top: 30px;">
         <p style="font-size: 18px; color: #555;">
