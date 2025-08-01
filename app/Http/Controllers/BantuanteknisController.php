97d1251a8ca20c108681a52a2b13c97202072ffa
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-02 00:40:34 +0700

    e

--- app/Http/Controllers/BantuanteknisController.php
+++ app/Http/Controllers/BantuanteknisController.php
@@ -2608,7 +2608,7 @@ public function bepersontimteknis(Request $request)
 {
     $user = Auth::user();
     $search = $request->input('search');
-    $perPage = $request->input('perPage', 20);
+    $perPage = $request->input('perPage', 10);
 
     // Query dasar: hanya data dengan jenispengajuanbantek_id = 1
     $query = bantuanteknis::whereHas('jenispengajuanbantek', function ($q) {
