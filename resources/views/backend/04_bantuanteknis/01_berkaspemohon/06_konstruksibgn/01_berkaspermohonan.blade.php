c352a86b72ba5552f3f10e37b6ed7b4fa460d3f2
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-02 00:02:08 +0700

    e

--- resources/views/backend/04_bantuanteknis/01_berkaspemohon/06_konstruksibgn/01_berkaspermohonan.blade.php
+++ resources/views/backend/04_bantuanteknis/01_berkaspemohon/06_konstruksibgn/01_berkaspermohonan.blade.php
@@ -96,7 +96,7 @@ th {
 
      <div class="container-fluid">
          <!--begin::Row-->
-         <div class="row" style="margin-right: 10px; margin-left:10px;">
+         <div class="putih row" style="margin-right: 10px; margin-left:10px;">
              <!-- /.card -->
              <div class="card mb-4">
                  {{-- <div class="card-header">
