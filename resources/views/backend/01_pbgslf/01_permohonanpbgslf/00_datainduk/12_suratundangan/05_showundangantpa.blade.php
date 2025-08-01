729e40e04013041f1815f9c14b843fbc0fcedc76
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-01 09:47:13 +0700

    e

--- resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/12_suratundangan/05_showundangantpa.blade.php
+++ resources/views/backend/01_pbgslf/01_permohonanpbgslf/00_datainduk/12_suratundangan/05_showundangantpa.blade.php
@@ -313,9 +313,9 @@
     <div class="kop" style="font-family: 'Times New Roman', serif !important;">
       <img src="/assets/abgblora/logo/logokabupatenblora.png" class="logo" style="float: left;">
       <div style="display: inline-block; font-family: 'Times New Roman', serif !important;">
-        <h3 style="font-family: 'Times New Roman', serif !important;">PEMERINTAH KABUPATEN BLORA</h3>
-        <h3 style="font-family: 'Times New Roman', serif !important;">DINAS PEKERJAAN UMUM DAN PENATAAN RUANG</h3>
-        <p style="font-family: 'Times New Roman', serif !important;">Jl. Nusantara No. 62 Telp. (0296) 531004</p>
+        <h3 style="font-family: 'Times New Roman', serif !important; font-size:18px;">PEMERINTAH KABUPATEN BLORA</h3>
+        <h3 style="font-family: 'Times New Roman', serif !important; font-size:18px;">DINAS PEKERJAAN UMUM DAN PENATAAN RUANG</h3>
+        <p style="font-family: 'Times New Roman', serif !important; font-size:18px;">Jl. Nusantara No. 62 Telp. (0296) 531004</p>
         <h3 style="font-family: 'Times New Roman', serif !important;">BLORA 58214</h3>
       </div>
       <div style="clear: both;"></div>
