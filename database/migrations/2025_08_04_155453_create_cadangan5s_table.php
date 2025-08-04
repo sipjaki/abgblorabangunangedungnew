7b63874fda6e7d4555edbbfec646d5e98075aa22
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-04 23:02:40 +0700

    e

---
+++ database/migrations/2025_08_04_155453_create_cadangan5s_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cadangan5s', function (Blueprint $table) {
+            $table->id();
+               $table->foreignId('kuncibaru_id')->index()->nullable();
+            $table->foreignId('kuncibaru1_id')->index()->nullable();
+            $table->string('cadangan1')->nullable();
+            $table->string('cadangan2')->nullable();
+            $table->string('cadangan3')->nullable();
+            $table->string('cadangan4')->nullable();
+            $table->string('cadangan5')->nullable();
+            $table->string('cadangan6')->nullable();
+            $table->string('cadangan7')->nullable();
+            $table->string('cadangan8')->nullable();
+            $table->string('cadangan9')->nullable();
+            $table->string('cadangan10')->nullable();
+
+            $table->text('catatan1')->nullable();
+            $table->text('catatan2')->nullable();
+
+            $table->softDeletes();
+         
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cadangan5s');
+    }
+};
