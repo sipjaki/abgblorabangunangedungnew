8db03494e910c87e5ee2e2ad94c8f455d9e34240
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-06 02:30:32 +0700

    e

--- resources/views/backend/00_administrator/00_baganterpisah/09_button.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/09_button.blade.php
@@ -758,7 +758,8 @@ document.querySelectorAll('input, textarea').forEach(input => {
     max-width: 800px;
     margin: 0 auto;
     box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
-    font-family: 'Segoe UI', sans-serif;
+font-family: 'Poppins', sans-serif;
+
   }
 
   .card-pbg h2 {
