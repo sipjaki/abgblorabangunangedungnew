567cee0bc3b43fb277519d291cc91acbe3762789
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-06 02:21:48 +0700

    e

--- resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
@@ -1323,7 +1323,9 @@
 <li class="nav-item">
 
 <a href="#" class="nav-link" style="display: flex; align-items: center;">
-  <i class="bi bi-person-search nav-icon" style="color: white; font-size: 16px; flex-shrink: 0;"></i>
+  {{-- <i class="bi bi-person-search nav-icon" style="color: white; font-size: 16px; flex-shrink: 0;"></i> --}}
+  <i class="bi bi-clipboard-check nav-icon" style="color: white; font-size: 16px; flex-shrink: 0;"></i>
+
   <p style="color: white; margin: 0 0 0 8px; flex-grow: 1;">Penilik</p>
   <i class="nav-arrow bi bi-chevron-right" style="color: white; margin-left: auto;"></i>
 </a>
