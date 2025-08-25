cbfaf9e068130dc9d3fcda777077b8c3f7a92537
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-25 15:23:37 +0700

    e

--- resources/views/backend/02_pendataanbangunangedung/01_databaseutama/02_databangunangedungnew.blade.php
+++ resources/views/backend/02_pendataanbangunangedung/01_databaseutama/02_databangunangedungnew.blade.php
@@ -234,7 +234,7 @@ th {
                             function searchTable() {
                             let input = document.getElementById("searchInput").value;
 
-                            fetch(`/bepbgslfindexslf?search=${input}`)
+                            fetch(`/bebangunangedung?search=${input}`)
                                 .then(response => response.text())
                                 .then(html => {
                                     let parser = new DOMParser();
