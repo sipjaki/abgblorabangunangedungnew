8dd7dabdfca193b7b9ece92ae62560e9bfea510b
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-28 09:32:14 +0700

    e

--- resources/views/backend/02_pendataanbangunangedung/02_datatanah/01_databgtanah.blade.php
+++ resources/views/backend/02_pendataanbangunangedung/02_datatanah/01_databgtanah.blade.php
@@ -310,12 +310,12 @@ th {
                         {{-- Khusus tampilkan Catatan jika Pilihan Catatan = tidak lengkap --}}
 
 
-                      {{-- <a href="/bedatabgprofiltanahupdate/{{ $pemilik->id }}">
+                      <a href="/bedatabgprofiltanahupdate/{{ $pemilik->id }}">
                             <p class="button-berkas">
                                 <i class="bi bi-pencil-square" style="margin-right: 6px; color: navy;"></i>
                                 Perbaikan Data
                             </p>
-                        </a> --}}
+                        </a>
 
 
                      @if (strtolower($pemilik->pilihancatatan) === 'tidak lengkap')
