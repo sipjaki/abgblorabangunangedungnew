62093a0eadb9ab2eb9919f64d1538b50ebad95a8
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-28 14:33:38 +0700

    e

--- app/Http/Controllers/PbgslfController.php
+++ app/Http/Controllers/PbgslfController.php
@@ -1701,7 +1701,7 @@ public function bepbgsuratpemberitahuanshow(Request $request, $id)
     $user = Auth::user();
     // Cari data pbg berdasarkan ID
     $data = pbgslfbangunan::findOrFail($id);
-    $surat = suratpemberitahuanpbg::findOrFail($id);
+    $surat = suratpemberitahuanpbg::firstOrNew(['pbgslfbangunan_id' => $id]);
     $datapemilik = datapemilik::firstOrNew(['pbgslfbangunan_id' => $id]);
 
     return view('backend.01_pbgslf.01_permohonanpbgslf.00_datainduk.09_suratpemberitahuan.02_showsurat', [
