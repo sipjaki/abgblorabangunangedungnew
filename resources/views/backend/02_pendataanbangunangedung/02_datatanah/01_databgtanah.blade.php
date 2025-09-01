df62c18f95ac2641452694de000d8404f1799470
SipjakiBlora <sipjakiblora@gmail.com>
2025-09-01 14:56:16 +0700

    ergg

--- resources/views/backend/02_pendataanbangunangedung/02_datatanah/01_databgtanah.blade.php
+++ resources/views/backend/02_pendataanbangunangedung/02_datatanah/01_databgtanah.blade.php
@@ -341,15 +341,16 @@ th {
     </div>
 @endif
 
-
+<br>
                      <a href="javascript:void(0)" title="Delete"
    data-bs-toggle="modal" data-bs-target="#deleteModal"
    data-judul="{{ $pemilik->id }}"
    onclick="setDeleteUrl(this)"
    style="text-decoration: none;"
 >
-   <i class="bi bi-trash" style="color: red; margin-right:4px;"></i>
-   <span style="color: red;">Hapus</span>
+<span style="color: white;">
+   <i class="bi bi-trash" style="color: white; margin-right:4px;"></i>
+    Hapus</span>
 </a>
 
                     </div>
