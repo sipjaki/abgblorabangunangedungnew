08e50f455485610dca50610f2709ccbacb2128fc
SipjakiBlora <sipjakiblora@gmail.com>
2025-09-02 15:30:38 +0700

    er

--- resources/views/backend/02_pendataanbangunangedung/04_klasfikasi/01_dataklasifikasi.blade.php
+++ resources/views/backend/02_pendataanbangunangedung/04_klasfikasi/01_dataklasifikasi.blade.php
@@ -462,7 +462,7 @@ function previewPDF(event, containerId, iframeId, messageId) {
                  function setDeleteUrl(button) {
                      var id = button.getAttribute('data-judul');
                      document.getElementById('itemName').innerText = id;
-                     var deleteUrl = "/bepbgdatapemilikdelete/" + encodeURIComponent(id);
+                     var deleteUrl = "/bedatabgklasifikasidelete/" + encodeURIComponent(id);
                      document.getElementById('deleteForm').action = deleteUrl;
                  }
                  </script>
