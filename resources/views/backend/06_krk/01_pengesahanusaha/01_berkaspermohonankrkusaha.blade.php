41f8ede6b338b117c792babe58c09f2af1592470
SipjakiBlora <sipjakiblora@gmail.com>
2025-10-14 13:52:01 +0700

    e

--- resources/views/backend/06_krk/01_pengesahanusaha/01_berkaspermohonankrkusaha.blade.php
+++ resources/views/backend/06_krk/01_pengesahanusaha/01_berkaspermohonankrkusaha.blade.php
@@ -687,6 +687,12 @@ th {
     <td style="text-align: center;">:</td>
     <td style="text-align: left;">{{ $data->kabupaten ?? '-' }}</td>
 </tr>
+<tr>
+    <td style="text-align: center;">15</td>
+    <td style="text-align: left;"><i class="bi bi-globe" style="margin-right:6px;"></i> Koordinat Lokasi</td>
+    <td style="text-align: center;">:</td>
+    <td style="text-align: left;">{{ $data->koordinatlokasi ?? '-' }}</td>
+</tr>
 
 
 </table>
