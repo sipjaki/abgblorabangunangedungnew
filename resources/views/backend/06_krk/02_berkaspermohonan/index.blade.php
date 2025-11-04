c39593c8329bd3e46dc8d55057d145df44489e1c
SipjakiBlora <sipjakiblora@gmail.com>
2025-11-04 09:14:47 +0700

    erge

--- resources/views/backend/06_krk/02_berkaspermohonan/index.blade.php
+++ resources/views/backend/06_krk/02_berkaspermohonan/index.blade.php
@@ -521,102 +521,121 @@
         }
     </style>
 
-  <div class="row">
-    <div class="col-md-3 col-sm-6 col-12">
-        <a href="/bekrkusaha">
-            <div class="info-box shadow-lg rounded-3 p-4" style="background: #000080; color: white; transition: all 0.3s ease;"
-                 onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.querySelector('.info-box-text').style.color='black'; this.querySelector('.info-box-number').style.color='black';"
-                 onmouseout="this.style.backgroundColor='#000080'; this.style.color='white'; this.querySelector('.info-box-text').style.color='white'; this.querySelector('.info-box-number').style.color='white';">
-                <span class="info-box-icon d-flex justify-content-center align-items-center p-3 shadow-sm rounded" style="background-color: #ffd100; width: 60px; height: 60px;">
-                    <!-- SVG icon for Fungsi Usaha -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="green" class="bi bi-briefcase" viewBox="0 0 16 16">
-                        <path d="M0 4a2 2 0 0 1 2-2h3V1a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v1h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zM4 3V2h8v1H4zm10 1H2v8h12V4z"/>
-                    </svg>
-                </span>
-                <div class="info-box-content mt-3 text-center" style="font-family: 'Poppins', sans-serif;">
-                    <span class="info-box-text" style="color: white;">Fungsi Usaha</span>
-                    <span class="info-box-number fw-bold" style="font-size: 16px;">{{$datajumlahkrkusaha}} Permohonan</span>
+
+<div class="row">
+    <!-- 1. Permohonan PBG SLF -->
+    <div class="col-md-3 col-sm-6 col-12" style="margin-top: 5px; margin-bottom:5px;">
+        <a href="/bekrkusaha" style="text-decoration: none;">
+            <div class="dashboard-card card-1">
+                <div class="card-content">
+                    <div class="number-container">
+                        <img src="/assets/icon/pupr.png" alt="icon" width="40">
+                    </div>
+                    <div class="info-content">
+                        <p class="info-text">
+                            <i class="bi bi-file-earmark-check" style="margin-right: 5px;"></i>
+                            <span class="info-number">{{$datajumlahkrkusaha}}</span>
+                        </p>
+                        <p class="info-text">
+                            Permohonan KRK Usaha
+                        </p>
+                        <p class="small-text">Informasi KRK Usaha</p>
+                    </div>
                 </div>
             </div>
         </a>
     </div>
 
-    <div class="col-md-3 col-sm-6 col-12">
-        <a href="/bekrkhunian">
-            <div class="info-box shadow-lg rounded-3 p-4" style="background: #000080; color: white; transition: all 0.3s ease;"
-                 onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.querySelector('.info-box-text').style.color='black'; this.querySelector('.info-box-number').style.color='black';"
-                 onmouseout="this.style.backgroundColor='#000080'; this.style.color='white'; this.querySelector('.info-box-text').style.color='white'; this.querySelector('.info-box-number').style.color='white';">
-                <span class="info-box-icon d-flex justify-content-center align-items-center p-3 shadow-sm rounded" style="background-color: #ffd100; width: 60px; height: 60px;">
-                    <!-- SVG icon for Fungsi Hunian -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="green" class="bi bi-house-door" viewBox="0 0 16 16">
-                        <path d="M8 3.293l6 6V13h-2v-2a1 1 0 0 0-1-1h-2V6h-4v3H3a1 1 0 0 0-1 1v2H0v-3l8-7.707zM8 1.5l7 7V14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8.5l7-7z"/>
-                    </svg>
-                </span>
-                <div class="info-box-content mt-3 text-center" style="font-family: 'Poppins', sans-serif;">
-                    <span class="info-box-text" style="color: white;">Fungsi Hunian</span>
-                    <span class="info-box-number fw-bold" style="font-size: 16px;">{{$datajumlahkrkhunian}} Permohonan</span>
+    <!-- 2. Pendataan Bangunan Gedung -->
+    <div class="col-md-3 col-sm-6 col-12" style="margin-top: 5px; margin-bottom:5px;">
+        <a href="/bekrkhunian" style="text-decoration: none;">
+            <div class="dashboard-card card-1">
+                <div class="card-content">
+                    <div class="number-container">
+                        <img src="/assets/icon/pupr.png" alt="icon" width="40">
+                    </div>
+                    <div class="info-content">
+                        <p class="info-text">
+                            <i class="bi bi-building-check" style="margin-right: 5px;"></i>
+                            <span class="info-number">{{$datajumlahkrkhunian}} </span>
+                        </p>
+                        <p class="info-text">
+                            Permohonan KRK Hunian
+                        </p>
+                        <p class="small-text">Informasi KRK Hunian</p>
+                    </div>
                 </div>
             </div>
         </a>
     </div>
 
-    <!-- Fungsi Keagamaan -->
-    <div class="col-md-3 col-sm-6 col-12">
-        <a href="/bekrkkeagamaan">
-            <div class="info-box shadow-lg rounded-3 p-4" style="background: #000080; color: white; transition: all 0.3s ease;"
-                 onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.querySelector('.info-box-text').style.color='black'; this.querySelector('.info-box-number').style.color='black';"
-                 onmouseout="this.style.backgroundColor='#000080'; this.style.color='white'; this.querySelector('.info-box-text').style.color='white'; this.querySelector('.info-box-number').style.color='white';">
-                <span class="info-box-icon d-flex justify-content-center align-items-center p-3 shadow-sm rounded" style="background-color: #ffd100; width: 60px; height: 60px;">
-                    <!-- SVG icon for Fungsi Keagamaan -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="green" class="bi bi-church" viewBox="0 0 16 16">
-                        <path d="M8 0L6 2h2v5H4V2h2l2-2h2l2 2h2v5h-6V2h2L8 0zM3 7v8h1V7H3zm10 0v8h-1V7h1z"/>
-                    </svg>
-                </span>
-                <div class="info-box-content mt-3 text-center" style="font-family: 'Poppins', sans-serif;">
-                    <span class="info-box-text" style="color: white;">Fungsi Keagamaan</span>
-                    <span class="info-box-number fw-bold" style="font-size: 16px;">{{$datajumlahkrkkeagamaan}} Permohonan</span>
+    <!-- 3. Bantuan Teknis -->
+    <div class="col-md-3 col-sm-6 col-12" style="margin-top: 5px; margin-bottom:5px;">
+        <a href="/bekrkkeagamaan" style="text-decoration: none;">
+            <div class="dashboard-card card-1">
+                <div class="card-content">
+                    <div class="number-container">
+                        <img src="/assets/icon/pupr.png" alt="icon" width="40">
+                    </div>
+                    <div class="info-content">
+                        <p class="info-text">
+                            <i class="bi bi-tools" style="margin-right: 5px;"></i>
+                            <span class="info-number">{{$datajumlahkrkkeagamaan}} </span>
+                        </p>
+                        <p class="info-text">
+                            Permohonan KRK Keagamaan
+                        </p>
+                        <p class="small-text">Informasi KRK Keagamaan</p>
+                    </div>
                 </div>
             </div>
         </a>
     </div>
 
-    <!-- Fungsi Sosial Budaya -->
-    <div class="col-md-3 col-sm-6 col-12">
-        <a href="/bekrksosbud">
-            <div class="info-box shadow-lg rounded-3 p-4" style="background: #000080; color: white; transition: all 0.3s ease;"
-                 onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.querySelector('.info-box-text').style.color='black'; this.querySelector('.info-box-number').style.color='black';"
-                 onmouseout="this.style.backgroundColor='#000080'; this.style.color='white'; this.querySelector('.info-box-text').style.color='white'; this.querySelector('.info-box-number').style.color='white';">
-                <span class="info-box-icon d-flex justify-content-center align-items-center p-3 shadow-sm rounded" style="background-color: #ffd100; width: 60px; height: 60px;">
-                    <!-- SVG icon for Fungsi Sosial Budaya -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="green" class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M3 14s-1 0-1-1 1-3 4-3 4 3 4 3-1 1-1 1H3z"/>
-                        <path d="M8 0a7 7 0 1 1 0 14A7 7 0 0 1 8 0zM8 1a6 6 0 1 0 0 12A6 6 0 0 0 8 1z"/>
-                    </svg>
-                </span>
-                <div class="info-box-content mt-3 text-center" style="font-family: 'Poppins', sans-serif;">
-                    <span class="info-box-text" style="color: white;">Fungsi Sosial Budaya</span>
-                    <span class="info-box-number fw-bold" style="font-size: 16px;">{{$datajumlahkrksosbud}} Permohonan</span>
+    <!-- 4. Keterangan Rencana Kabupaten -->
+    <div class="col-md-3 col-sm-6 col-12" style="margin-top: 5px; margin-bottom:5px;">
+        <a href="/bekrksosbud" style="text-decoration: none;">
+            <div class="dashboard-card card-1">
+                <div class="card-content">
+                    <div class="number-container">
+                        <img src="/assets/icon/pupr.png" alt="icon" width="40">
+                    </div>
+                    <div class="info-content">
+                        <p class="info-text">
+                            <i class="bi bi-geo-alt" style="margin-right: 5px;"></i>
+                            <span class="info-number">{{$datajumlahkrksosbud}}</span>
+                        </p>
+                        <p class="info-text">
+                            Permohonan KRK Sosial Budaya
+                        </p>
+                        <p class="small-text">Informasi KRK Sosial Budaya</p>
+                    </div>
                 </div>
             </div>
         </a>
     </div>
+</div>
 
-    <!-- Fungsi Sosial Budaya -->
-    <div class="col-md-3 col-sm-6 col-12">
-        <a href="/bekrkmenaratelkom">
-            <div class="info-box shadow-lg rounded-3 p-4" style="background: #000080; color: white; transition: all 0.3s ease;"
-                 onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.querySelector('.info-box-text').style.color='black'; this.querySelector('.info-box-number').style.color='black';"
-                 onmouseout="this.style.backgroundColor='#000080'; this.style.color='white'; this.querySelector('.info-box-text').style.color='white'; this.querySelector('.info-box-number').style.color='white';">
-                <span class="info-box-icon d-flex justify-content-center align-items-center p-3 shadow-sm rounded" style="background-color: #ffd100; width: 60px; height: 60px;">
-                    <!-- SVG icon for Fungsi Sosial Budaya -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="green" class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M3 14s-1 0-1-1 1-3 4-3 4 3 4 3-1 1-1 1H3z"/>
-                        <path d="M8 0a7 7 0 1 1 0 14A7 7 0 0 1 8 0zM8 1a6 6 0 1 0 0 12A6 6 0 0 0 8 1z"/>
-                    </svg>
-                </span>
-                <div class="info-box-content mt-3 text-center" style="font-family: 'Poppins', sans-serif;">
-                    <span class="info-box-text" style="color: white;">Menara Telekomunikasi</span>
-                    <span class="info-box-number fw-bold" style="font-size: 16px;">{{$datajumlahkrkmenara}} Permohonan</span>
+
+<div class="row">
+    <!-- 4. Keterangan Rencana Kabupaten -->
+    <div class="col-md-3 col-sm-6 col-12" style="margin-top: 5px; margin-bottom:5px;">
+        <a href="/bekrkmenaratelkom" style="text-decoration: none;">
+            <div class="dashboard-card card-1">
+                <div class="card-content">
+                    <div class="number-container">
+                        <img src="/assets/icon/pupr.png" alt="icon" width="40">
+                    </div>
+                    <div class="info-content">
+                        <p class="info-text">
+                            <i class="bi bi-geo-alt" style="margin-right: 5px;"></i>
+                            <span class="info-number">{{$datajumlahkrkmenara}}</span>
+                        </p>
+                        <p class="info-text">
+                            Permohonan KRK Menara Telekomunikasi
+                        </p>
+                        <p class="small-text">Informasi KRK Menara Telekomunikasi</p>
+                    </div>
                 </div>
             </div>
         </a>
