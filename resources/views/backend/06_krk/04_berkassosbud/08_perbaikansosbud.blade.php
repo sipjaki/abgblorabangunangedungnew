6248460316f02e0696ecca740074f12642589977
SipjakiBlora <sipjakiblora@gmail.com>
2025-11-07 19:57:42 +0700

    [perubau

--- resources/views/backend/06_krk/04_berkassosbud/08_perbaikansosbud.blade.php
+++ resources/views/backend/06_krk/04_berkassosbud/08_perbaikansosbud.blade.php
@@ -421,7 +421,7 @@ function previewPDF(event, containerId, iframeId, messageId) {
 <br><br>
                             <div style="display: flex; justify-content: flex-end; margin-bottom:20px;">
                                 <div class="flex justify-end">
-                               <button class="button-modern" type="button" onclick="openModal()">
+                               <button class="button-berkas" type="button" onclick="openModal()">
                                     <i class="bi bi-save" style="margin-right: 5px;"></i>
                                     <span style="font-family: 'Poppins', sans-serif;">Simpan Perbaikan ? </span>
                                     </button>
