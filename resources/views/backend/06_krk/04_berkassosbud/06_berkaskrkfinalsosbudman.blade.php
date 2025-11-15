030dddd9aa4d3cc3adba22ca39e180d65f06911f
SipjakiBlora <sipjakiblora@gmail.com>
2025-11-15 12:47:05 +0700

    perubahan luas lahan

--- resources/views/backend/06_krk/04_berkassosbud/06_berkaskrkfinalsosbudman.blade.php
+++ resources/views/backend/06_krk/04_berkassosbud/06_berkaskrkfinalsosbudman.blade.php
@@ -270,7 +270,7 @@
     <td style="text-align: center; border: 1px solid #ddd; padding: 6px; font-size: 14px; font-family: 'Times New Roman', Times, serif !important;">:</td>
     <td style="text-align: left; border: 1px solid #ddd; padding: 6px; white-space: normal; word-wrap: break-word; overflow-wrap: break-word; font-size: 14px; font-family: 'Times New Roman', Times, serif !important;">
         {{-- {{ $data->koordinatlokasi ?? '-' }} --}}
-        {{ $item->luaslahan ? $item->luaslahan . ' M²' : '-' }}
+        {{ $data->luastanah ? $data->luastanah . ' M²' : '-' }}
 
     </td>
 </tr>
