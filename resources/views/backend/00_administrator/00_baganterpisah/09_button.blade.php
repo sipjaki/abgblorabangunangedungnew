1ee321d5d491fa046f0c231f73415d9018aca454
SipjakiBlora <sipjakiblora@gmail.com>
2026-01-20 15:34:02 +0700

    g

--- resources/views/backend/00_administrator/00_baganterpisah/09_button.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/09_button.blade.php
@@ -9,7 +9,7 @@
 
 .button-modern {
     background: linear-gradient(145deg, #f9fafc, #eef3fb);
-    color: #1e293b;
+    color: #5998fd;
     border: 1px solid #d0d7e2;
     padding: 10px 20px;
     border-radius: 10px;
