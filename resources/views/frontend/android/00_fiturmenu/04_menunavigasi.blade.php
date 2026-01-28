a9ed2ecab5118faf57736245ea08fbfc67a537c0
SipjakiBlora <sipjakiblora@gmail.com>
2026-01-29 03:18:18 +0700

    erehg

--- resources/views/frontend/android/00_fiturmenu/04_menunavigasi.blade.php
+++ resources/views/frontend/android/00_fiturmenu/04_menunavigasi.blade.php
@@ -1,6 +1,6 @@
 
 
-<div id="Feature" class="px-[18px] relative z-10">
+<div id="Feature" class="px-[18px] relative z-10" style="margin-top: -200px;">
     {{-- <div
   class="relative p-[18px_16px] rounded-xl overflow-hidden grid grid-cols-4 gap-[27px]"
   style="
