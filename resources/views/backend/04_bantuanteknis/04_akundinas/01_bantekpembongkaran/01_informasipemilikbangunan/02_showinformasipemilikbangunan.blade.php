ee9ccb30a10e9649d1b0fe8c1e49a6b9c251be60
SipjakiBlora <sipjakiblora@gmail.com>
2026-01-30 02:55:04 +0700

    e

--- resources/views/backend/04_bantuanteknis/04_akundinas/01_bantekpembongkaran/01_informasipemilikbangunan/02_showinformasipemilikbangunan.blade.php
+++ resources/views/backend/04_bantuanteknis/04_akundinas/01_bantekpembongkaran/01_informasipemilikbangunan/02_showinformasipemilikbangunan.blade.php
@@ -445,16 +445,14 @@
     <!-- BUTTON AKSI -->
     <div class="d-flex gap-2">
         <!-- KEMBALI -->
-        <a href="{{ route(
-            'bebantekpembongkaranshow',
-            [
-                urlencode($data->namapemilik),
-                $data->id
-            ]
-        ) }}"
-        class="button-modern">
-            <i class="bi bi-arrow-left me-1"></i> Kembali
-        </a>
+       <a href="{{ route('bebantekpembongkaranshow', [
+        'namapemilik' => urlencode($data->namapemilik),
+        'id' => $data->id
+    ]) }}"
+    class="button-modern">
+    <i class="bi bi-arrow-left me-1"></i> Kembali
+</a>
+
 
         <!-- KEMBALI KE DATA DASAR -->
         <a href="{{ route('bebantekpembongkaran') }}"
