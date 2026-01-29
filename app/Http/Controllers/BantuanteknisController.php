b19e6d91477e0e6fd5659909d061c529f513e53c
SipjakiBlora <sipjakiblora@gmail.com>
2026-01-30 05:32:54 +0700

    erg

--- app/Http/Controllers/BantuanteknisController.php
+++ app/Http/Controllers/BantuanteknisController.php
@@ -6634,7 +6634,7 @@ public function informasipemilikbangunan($namapemilik, $id)
         // VALIDASI SEMUA FIELD
         $validated = $request->validate([
             // DATA SURAT PERMOHONAN
-            'bantekpembongkaraninduk_id' => 'nullable|string',
+            'bantekpembongkaraninduk_id' => 'nullable|integer',
             'nosurat' => 'nullable|string|max:255',
             'tanggalsurat' => 'nullable|date',
             'suratpermohonan' => 'nullable|file|mimes:pdf,jpg,jpeg,png,docx|max:15360', // 15MB
