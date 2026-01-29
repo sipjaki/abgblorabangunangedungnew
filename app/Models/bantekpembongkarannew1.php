23782d891e910a029554d4c96800016e6b4b732c
SipjakiBlora <sipjakiblora@gmail.com>
2026-01-30 03:44:38 +0700

    erg

--- app/Models/bantekpembongkarannew1.php
+++ app/Models/bantekpembongkarannew1.php
@@ -14,6 +14,6 @@ class bantekpembongkarannew1 extends Model
 
     public function bantekpembongkaraninduk()
     {
-        return $this->belongsTo(bantekpembongkaraninduk::class, 'bantekpembongkaraninduk_id');
+        return $this->hasOne(bantekpembongkaraninduk::class, 'bantekpembongkaraninduk_id');
     }
 }
