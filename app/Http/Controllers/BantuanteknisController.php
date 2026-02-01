354bd62792b0e030799f8cfb12dc618f63d7871f
SipjakiBlora <sipjakiblora@gmail.com>
2026-02-02 03:22:02 +0700

    ergregre

--- app/Http/Controllers/BantuanteknisController.php
+++ app/Http/Controllers/BantuanteknisController.php
@@ -7185,9 +7185,9 @@ public function bebantekbangunanbongkarcrnew(Request $request)
         'berkaspemeriksaan' => 'nullable|file|mimes:pdf,jpg,jpeg,png,docx|max:15360',
 
         // Cadangan
-        'cadangan1' => 'nullable|file|mimes:pdf,jpg,jpeg,png,docx|max:15360',
+        'cadangan1' => 'nullable|string',
         'cadangan2' => 'nullable|string',
-        'cadangan3' => 'nullable|string',
+        'cadangan3' => 'nullable|file|mimes:pdf,jpg,jpeg,png,docx|max:15360',
     ]);
 
     // ==============================
