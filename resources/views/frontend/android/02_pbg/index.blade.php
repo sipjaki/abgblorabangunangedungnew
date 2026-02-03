ee0f5a1b38621055f7e8df4da8da03e72c63a7ec
SipjakiBlora <sipjakiblora@gmail.com>
2026-02-03 07:33:54 +0700

    e

--- resources/views/frontend/android/02_pbg/index.blade.php
+++ resources/views/frontend/android/02_pbg/index.blade.php
@@ -1,4 +1,5 @@
 @include('frontend.android.00_fiturmenu.01_header')
+@include('backend.00_administrator.00_baganterpisah.09_button')
 
 <body class="font-poppins text-[#070625]">
   <section id="content" class="max-w-[640px] w-full min-h-screen mx-auto flex flex-col bg-[#F8F8F8] overflow-x-hidden pb-[120px] relative">
