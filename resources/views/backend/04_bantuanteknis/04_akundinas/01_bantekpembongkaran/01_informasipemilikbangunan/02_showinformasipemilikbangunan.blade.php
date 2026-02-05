d63ccfe2ec1de3a7d06d47fd34030aa1b1dd8dbf
SipjakiBlora <sipjakiblora@gmail.com>
2026-02-05 14:39:08 +0700

    ergr

--- resources/views/backend/04_bantuanteknis/04_akundinas/01_bantekpembongkaran/01_informasipemilikbangunan/02_showinformasipemilikbangunan.blade.php
+++ resources/views/backend/04_bantuanteknis/04_akundinas/01_bantekpembongkaran/01_informasipemilikbangunan/02_showinformasipemilikbangunan.blade.php
@@ -800,20 +800,21 @@
                 <div class="section-header">
                     <i class="bi bi-globe-asia-australia"></i> Alasan Pembongkaran Bangunan Gedung
                 </div>
-            <div class="section-content">
-    <div class="row">
-        <div class="col-12">
-            <div class="data-card">
-                <div class="data-label">
-                    <i class="bi bi-rulers"></i> Alasan Pembongkaran
+                 <div class="section-content">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="data-card">
+                                <div class="data-label">
+                                    <i class="bi bi-rulers"></i> Alasan Pembongkaran
+                                </div>
+                                <div class="data-value">
+                                    {{ $data->cadangan2 ?? 'Data Belum Di Perbarui, Silahkan Lakukan Perbaikan' }} m²
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="data-value">
-                    {{ $data->cadangan2 ?? 'Data Belum Di Perbarui, Silahkan Lakukan Perbaikan' }} m²
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
 
      <div class="container-fluid">
          <div class="row">
