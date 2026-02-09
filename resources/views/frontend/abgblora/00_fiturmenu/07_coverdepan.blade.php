c3536b914170cf77703c6efde8b3d18a9faa4a05
SipjakiBlora <sipjakiblora@gmail.com>
2026-02-09 13:20:17 +0700

    perubahan yang tera

---
+++ resources/views/frontend/abgblora/00_fiturmenu/07_coverdepan.blade.php
@@ -0,0 +1,46 @@
+<div class="w-full h-[190] absolute top-0 overflow-hidden">
+  <img src="/assets/android/iconmenu/temabaru.png" alt="Bangunan Blora" class="w-full h-full object-cover" />
+</div>
+
+     <div class="relative z-10 flex flex-col gap-6 mt-[60px]">
+
+<div style="
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 20px;
+  padding: 20px;
+  backdrop-filter: blur(2px);
+  -webkit-backdrop-filter: blur(4px);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+">
+  <!-- Logo Kiri -->
+  <div style="width: 42px; height: 42px; flex-shrink: 0;">
+    <img src="/assets/abgblora/logo/logokabupatenblora.png" alt="icon" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
+
+  <!-- Teks Tengah -->
+  <p style="
+    font-size: 15px;
+    font-weight: 500;
+    line-height: 22px;
+    color: #000;
+    text-align: center;
+    margin: 0;
+    flex: 1;
+  ">
+    Dinas Pekerjaan Umum <br>
+    Dan Penataan Ruang <br>
+    Kabupaten Blora
+  </p>
+
+  <!-- Logo Kanan -->
+  <div style="width: 42px; height: 42px; flex-shrink: 0;">
+    <img src="/assets/abgblora/logo/pupr.png" alt="icon" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
+</div>
--- resources/views/frontend/android/05_sosialisasi/00_halamansosialisasi.blade.php
+++ resources/views/frontend/android/05_sosialisasi/00_halamansosialisasi.blade.php
@@ -3,53 +3,8 @@
 @include('frontend.android.00_fiturmenu.06_alert')
 
 <body class="font-poppins text-[#070625]">
-  <section id="content" class="max-w-[640px] w-full min-h-screen mx-auto flex flex-col bg-[#F8F8F8] overflow-x-hidden pb-[120px] relative">
-<div class="w-full h-[190] absolute top-0 overflow-hidden">
-  <img src="/assets/android/iconmenu/temabaru.png" alt="Bangunan Blora" class="w-full h-full object-cover" />
-</div>
-
-     <div class="relative z-10 flex flex-col gap-6 mt-[60px]">
-
-<div style="
-  width: 100%;
-  max-width: 400px;
-  margin: 0 auto;
-  background-color: rgba(255, 255, 255, 0.7);
-  border-radius: 20px;
-  padding: 20px;
-  backdrop-filter: blur(2px);
-  -webkit-backdrop-filter: blur(4px);
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 10px;
-">
-  <!-- Logo Kiri -->
-  <div style="width: 42px; height: 42px; flex-shrink: 0;">
-    <img src="/assets/abgblora/logo/logokabupatenblora.png" alt="icon" style="width: 100%; height: 100%; object-fit: contain;">
-  </div>
-
-  <!-- Teks Tengah -->
-  <p style="
-    font-size: 15px;
-    font-weight: 500;
-    line-height: 22px;
-    color: #000;
-    text-align: center;
-    margin: 0;
-    flex: 1;
-  ">
-    Dinas Pekerjaan Umum <br>
-    Dan Penataan Ruang <br>
-    Kabupaten Blora
-  </p>
-
-  <!-- Logo Kanan -->
-  <div style="width: 42px; height: 42px; flex-shrink: 0;">
-    <img src="/assets/abgblora/logo/pupr.png" alt="icon" style="width: 100%; height: 100%; object-fit: contain;">
-  </div>
-</div>
+    <section id="content" class="max-w-[640px] w-full min-h-screen mx-auto flex flex-col bg-[#F8F8F8] overflow-x-hidden pb-[120px] relative">
+      @include('frontend.android.00_fiturmenu.07_coverdepan')
 
       <form action="success.html" id="Details" class="group result-card-container flex flex-col gap-6">
         <div id="Contact-details" class="bg-white rounded-xl overflow-hidden flex flex-col mx-[18px]">
