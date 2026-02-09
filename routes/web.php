7c144922ea4f9e4efd5e37c7f08187db725fa098
SipjakiBlora <sipjakiblora@gmail.com>
2026-02-09 13:11:46 +0700

    e

--- routes/web.php
+++ routes/web.php
@@ -962,7 +962,7 @@ Route::get('/daftaragenda/{id}', [AgendapelatihanabgController::class, 'daftarag
 Route::post('/pendaftaranpesertanew', [AgendapelatihanabgController::class, 'pendaftaranpesertanew'])->name('pendaftaranpesertanew');
 Route::get('/beagendapelatihanabg', [AgendapelatihanabgController::class, 'beagendapelatihanabg'])->middleware('auth')->name('beagendapelatihanabg');
 
-Route::get('/resagendapelatihan/{id}', [AgendapelatihanabgController::class, 'resagendapelatihan'])->middleware('auth');
+Route::get('/resagendapelatihan/{id}', [AgendapelatihanabgController::class, 'resagendapelatihan']);
 
 // EDIT AGENDA PELATIHAN BRO
 Route::get('/beagendapelatihanedit/{id}', [AgendapelatihanabgController::class, 'beagendapelatihanedit'])->middleware('auth')->name('agendapelatihanedit');
