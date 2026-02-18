d4cfd3f7c6fda014377cefec3f5c3ed6c6b3bcfd
SipjakiBlora <sipjakiblora@gmail.com>
2026-02-18 11:23:37 +0700

    ergrg

--- app/Http/Controllers/DatabaseAbgController.php
+++ app/Http/Controllers/DatabaseAbgController.php
@@ -992,7 +992,7 @@ public function ttdkepaladinasbloracreatenew(Request $request)
     }
 
     // Kirim data ke view form pembuatan dokumentasi cek lapangan
-    return view('backend.99_databaseabg.01_gsbblora.03_updatekepaladinas', [
+    return view('backend.99_databaseabg.04_ttdkepaladinas.03_updatekepaladinas', [
         'title' => 'Perbaikan Data Kepala Dinas !',
         'data' => $databantuanteknis,
         'user' => Auth::user()
