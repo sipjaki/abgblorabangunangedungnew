93f9b7124ba7beaaf3c5a66a964cd3620813bcea
SipjakiBlora <sipjakiblora@gmail.com>
2026-02-25 09:53:17 +0700

    perubahn menu pbg warna merah

--- resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
@@ -868,7 +868,7 @@
     <!-- Ganti SVG dengan Bootstrap Icon -->
     <i class="bi bi-building nav-icon" style="color: white; font-size: 16px;"></i>
 
-    <p style="color: white; margin-left: 8px;">
+    <p style="color: red; margin-left: 8px;">
         PBG/SLF
         <i class="nav-arrow bi bi-chevron-right" style="margin-left: auto;"></i>
     </p>
