450e7a986cb690f918c90723be062e79f2e6105b
SipjakiBlora <sipjakiblora@gmail.com>
2026-03-13 01:14:21 +0700

    sidebar baru

--- resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/03_sidebar.blade.php
@@ -1732,7 +1732,7 @@ Menu Sementara
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" viewBox="0 0 24 24" style="flex-shrink: 0;">
     <path d="M21 16v-2l-8-5V3.5a1.5 1.5 0 0 0-3 0V9L2 14v2l8-2.5V19l-2 1.5v1L12 21l4-1.5v-1L14 19v-5.5l7 2.5z"/>
   </svg>
-  <p style="color: white; margin: 0 0 0 8px; flex-grow: 1;">Perjalanan Dinas</p>
+  <p style="color: green; margin: 0 0 0 8px; flex-grow: 1;">Perjalanan Dinas</p>
   <i class="nav-arrow bi bi-chevron-right" style="margin-left: auto; color: white;"></i>
 </a>
 
