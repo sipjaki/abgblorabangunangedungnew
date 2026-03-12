6be9e42ab8ced23f5cd50f295645b72524c9e2e3
SipjakiBlora <sipjakiblora@gmail.com>
2026-03-13 03:01:26 +0700

    erg

--- resources/views/frontend/ui2026/00_fiturmenu/01_header.blade.php
+++ resources/views/frontend/ui2026/00_fiturmenu/01_header.blade.php
@@ -1,19 +1,10 @@
-
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
-    <meta name="description" content="Sistem Informasi Penyelenggaraan Bangunan Gedung Kabupaten Blora - Pelayanan administrasi bangunan gedung yang modern dan terintegrasi">
-
-<meta name="keywords" content="Sistem Informasi Bangunan Gedung, PBG Kabupaten Blora, SLF Kabupaten Blora, Bangunan Gedung Indonesia, Sistem Bangunan Gedung PUPR, Kementerian PUPR, Administrasi Bangunan Gedung">
-
-<meta name="author" content="Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Blora | Build and Deploy By CV. Angkasa Raya Teknik (Bandung)">
-<meta name="robots" content="index, follow">
-
-<link rel="icon" href="/assets/abgblora/logo/logokabupatenblora.png" type="image/x-icon">
-
+    <meta name="description" content="Sistem Informasi Penyelenggaraan Bangunan Gedung Kabupaten Blora - Layanan PBG, SLF, dan Administrasi Bangunan">
 
     <!-- Google Fonts - Poppins -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -24,13 +15,14 @@
     <script src="https://unpkg.com/lucide@latest"></script>
 
     <!-- CSS Files -->
-   <link rel="stylesheet" href="{{ asset('2026/css/reset.css') }}">
-<link rel="stylesheet" href="{{ asset('2026/css/variables.css') }}">
-<link rel="stylesheet" href="{{ asset('2026/css/base.css') }}">
-<link rel="stylesheet" href="{{ asset('2026/css/components.css') }}">
-<link rel="stylesheet" href="{{ asset('2026/css/sections.css') }}">
-<link rel="stylesheet" href="{{ asset('2026/css/animations.css') }}">
-<link rel="stylesheet" href="{{ asset('2026/css/responsive.css') }}">
+    <link rel="stylesheet" href="/assets/2026/css/reset.css">
+    <link rel="stylesheet" href="/assets/2026/css/variables.css">
+    <link rel="stylesheet" href="/assets/2026/css/base.css">
+    <link rel="stylesheet" href="/assets/2026/css/components.css">
+    <link rel="stylesheet" href="/assets/2026/css/sections.css">
+    <link rel="stylesheet" href="/assets/2026/css/animations.css">
+    <link rel="stylesheet" href="/assets/2026/css/responsive.css">
 
+<link rel="icon" href="/assets/abgblora/logo/logokabupatenblora.png" type="image/x-icon">
 
 </head>
