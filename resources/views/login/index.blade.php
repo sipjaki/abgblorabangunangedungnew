e218491a148e8df00a36336b39ac4c7e28fabed9
SipjakiBlora <sipjakiblora@gmail.com>
2026-04-14 17:01:37 +0700

    rubah logo

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -6,7 +6,7 @@
     <title>Halaman Login !</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="icon" href="/assets/abgblora/logo/logokabupatenblora.png" type="image/x-icon">
+    <link rel="icon" href="/assets/abgblora/logo/bangunangedungnew.png" type="image/x-icon">
     <style>
         :root {
             /* Warna utama biru Persib */
