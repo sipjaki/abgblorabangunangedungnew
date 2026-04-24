dccde1b9d6619cf1ffe9f50906ed185490431f8f
SipjakiBlora <sipjakiblora@gmail.com>
2026-04-24 17:52:30 +0700

    erg

--- resources/views/backend/04_bantuanteknis/01_berkaspemohon/05_newberkaspemohonasistensi.blade.php
+++ resources/views/backend/04_bantuanteknis/01_berkaspemohon/05_newberkaspemohonasistensi.blade.php
@@ -236,7 +236,7 @@
 
 
             <td style="text-align: center;">
-                <a href="{{ route('beasistensishowberkas1.show', $item->id) }}"
+                <a href="{{ route('beasistensishowberkas1.show', encrypt($item->id)) }}"
                     class="button-modern">
                     <i class="bi bi-eye" style="margin-right: 5px;"></i> Lihat Permohonan
                 </a>
