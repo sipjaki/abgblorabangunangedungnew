aea50af19e3f25bbac371f36606781bd93a583aa
SipjakiBlora <sipjakiblora@gmail.com>
2026-04-28 11:14:45 +0700

    perubahan

--- resources/views/backend/04_bantuanteknis/01_berkaspemohon/07_berkasceklapanganasistensi.blade.php
+++ resources/views/backend/04_bantuanteknis/01_berkaspemohon/07_berkasceklapanganasistensi.blade.php
@@ -117,7 +117,7 @@
 </a>
         @endcanany
 
-                        @canany(['superadmin', 'admin'])
+                        @canany(['superadmin', 'admin', 'konsultanbantek'])
 <button class="button-baru" type="button"
     onclick="location.href='/bebantuanteknislapangancreate/{{ $data->id }}';"
     style="cursor: pointer; margin-left:10px; color:black;">
