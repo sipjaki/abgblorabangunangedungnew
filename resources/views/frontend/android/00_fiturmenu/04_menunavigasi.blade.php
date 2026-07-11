201fa3b1c4519e602b7857d4c6b642b7e86d5653
SipjakiBlora <sipjakiblora@gmail.com>
2026-07-11 23:45:27 +0700

    wrgwg

--- resources/views/frontend/android/00_fiturmenu/04_menunavigasi.blade.php
+++ resources/views/frontend/android/00_fiturmenu/04_menunavigasi.blade.php
@@ -59,7 +59,7 @@
       <a href="/permohonankrkusaha">
         <div class="flex flex-col items-center gap-2">
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/android/versi2/05.png" class="w-full h-full object-cover" alt="icon" loading="lazy">
+            <img src="/assets/android/versi2/08.png" class="w-full h-full object-cover" alt="icon" loading="lazy">
           </div>
           <p class="font-medium text-sm text-[#757C98] leading-[21px]">KRK</p>
         </div>
@@ -85,7 +85,7 @@
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/android/versi2/08.png" class="w-full h-full object-cover" alt="icon" loading="lazy">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Bantuan <br> Gambar</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Bantek Gambar</p>
         </div>
       </a>
 
